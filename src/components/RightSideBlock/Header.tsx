6340260196a41329334cfd25f0686f596ef085ed
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-21 23:15:22 +0400

    Header for right side

---
+++ src/components/RightSideBlock/Header.tsx
@@ -0,0 +1,59 @@
+import {
+  Button,
+  Grid,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import CloseIcon from "@material-ui/icons/Close";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+
+interface HeaderProps {
+  headerSubText?: string;
+  handleClick?: () => void;
+}
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    headerContainer: {
+      width: "100%",
+      height: "64px",
+      borderBottom: "1px solid rgb(233,227,230)",
+      padding: "0 16px",
+    },
+    headerTitle: {
+      fontWeight: 700,
+    },
+  })
+);
+
+export const Header: React.FC<HeaderProps> = ({
+  headerSubText,
+  handleClick,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      container
+      alignItems="center"
+      justify="space-between"
+      className={classes.headerContainer}
+    >
+      <Grid>
+        <Typography variant="subtitle2" className={classes.headerTitle}>
+          Details
+        </Typography>
+        <Typography>{headerSubText}</Typography>
+      </Grid>
+      <Tooltip title="Close">
+        <IconButton onClick={handleClick}>
+          <CloseIcon />
+        </IconButton>
+      </Tooltip>
+    </Grid>
+  );
+};
+
+export default Header;
