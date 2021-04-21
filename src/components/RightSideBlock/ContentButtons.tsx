ff2f509c84fef91603ea7f0676af684cdc81065c
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-21 23:15:00 +0400

    Buttons for right side

---
+++ src/components/RightSideBlock/ContentButtons.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { Grid, IconButton } from '@material-ui/core'
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
+import FindInPageOutlinedIcon from '@material-ui/icons/FindInPageOutlined';
+import CreateOutlinedIcon from '@material-ui/icons/CreateOutlined';
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
+
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    buttonsWrapper: {
+        padding: "18px 0",
+        borderBottom: "1px solid rgb(233,227,230)",
+    },
+    leaveButton: {
+        color: "#e01e5a",
+        "&:hover": {
+            backgroundColor: "#ff6a9824",
+        }
+    }
+  })
+);
+
+export const ContentButtons = () => {
+
+    const classes = useStyles();
+
+    return (
+        <Grid container className={classes.buttonsWrapper} wrap="wrap" justify="space-around" alignItems="center">
+            <IconButton><PersonAddOutlinedIcon />Add</IconButton>
+            <IconButton><FindInPageOutlinedIcon />Find</IconButton>
+            <IconButton><CreateOutlinedIcon />Rename</IconButton>
+            <IconButton className={classes.leaveButton}><ExitToAppOutlinedIcon />Leave aweeawea a</IconButton>
+        </Grid>
+    )
+}
