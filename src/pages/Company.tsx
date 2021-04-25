86b5836a2b0ad5828a2907d9a31463a6ebc1b705
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-25 15:57:01 +0400

    Overflow and heights update

--- src/pages/Company.tsx
+++ src/pages/Company.tsx
@@ -14,6 +14,7 @@ import {
   Divider,
   List,
   Link,
+  Box,
 } from "@material-ui/core";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 
@@ -24,6 +25,7 @@ import StarBorderIcon from "@material-ui/icons/StarBorder";
 import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import QuestionAnswerOutlinedIcon from "@material-ui/icons/QuestionAnswerOutlined";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 import { SimplePopover } from "../components/SimplePopover";
 import { CompanyMenuButton } from "../components/CompanyMenuButton";
@@ -35,14 +37,17 @@ import { DirectMessageListItem } from "../components/DirectMessageListItem";
 import { StyledBadge } from "../components/StyledBadge";
 import MessagePane from "../components/MessagePane";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
-import { useResize } from "../components/hooks/useResize";
 import { RightSideBlock } from "../components/RightSideBlock";
+import { SimpleModal } from "../components/SimpleModal";
+import { EditProfileModal } from "../components/EditProfileModal";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     companyWrapper: {
       height: "100vh",
-      overflowX: "hidden"
+    },
+    companyContentWrapper: {
+      height: "calc(100% - 38px)",
     },
     appBar: {
       height: 38,
@@ -108,13 +113,24 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     channelSidebar: {
       backgroundColor: "#FBFAF7",
-      height: "100vh",
+    },
+    channelSideBarItemsWrapper: {
+      overflowY: "auto",
+      overflowX: "hidden",
+      maxHeight: "calc(100% - 64px)",
+      "&::-webkit-scrollbar": {
+        width: "0.5em",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        backgroundColor: theme.palette.primary.light,
+        borderRadius: "4px",
+      },
     },
     workspace: {
-      height: "100vh",
+      height: "100%",
       borderLeft: "1px solid rgb(233,227,230)",
       borderRight: "1px solid rgb(233,227,230)",
-      maxHeight: "100vh",
+      maxHeight: "100%",
     },
     workspaceHeader: {
       borderBottom: "1px solid rgb(233,227,230)",
@@ -132,22 +148,41 @@ const useStyles = makeStyles((theme: Theme) =>
       fontSize: "1rem",
     },
     workspaceContentGrid: {
-      padding: 20,
-      display: "grid",
-      gridTemplateColumns: "1fr",
-      gridTemplateRows: "10fr 2fr",
-      height: "90vh",
+      height: "calc(100% - 64px)",
+      padding: "0 20px",
     },
     workspaceContentMessages: {
       overflowY: "auto",
+      "&::-webkit-scrollbar": {
+        width: "0.5em",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "rgba(29,28,29,.52)",
+        borderRadius: "4px",
+      },
     },
     textareaSize: {
       resize: "none",
       width: "100%",
     },
     message: {
-      wordWrap: "break-word"
-    }
+      wordWrap: "break-word",
+    },
+    avatarMenuHeader: {
+      textAlign: "center",
+    },
+    rightSideInfoWrapper: {
+      overflowY: "auto",
+      overflowX: "hidden",
+      maxHeight: "100%",
+      "&::-webkit-scrollbar": {
+        width: "0.5em",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "rgba(29,28,29,.52)",
+        borderRadius: "4px",
+      },
+    },
   })
 );
 
@@ -187,25 +222,70 @@ export const Company = () => {
               </Tooltip>
             </Grid>
             <Grid container justify="flex-end" xs={3}>
-              <StyledBadge
-                overlap="circle"
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "right",
-                }}
-                variant="dot"
+              <SimplePopover
+                opener={
+                  <StyledBadge
+                    overlap="circle"
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "right",
+                    }}
+                    variant="dot"
+                  >
+                    <Avatar
+                      className={classes.avatar}
+                      alt="Remy Sharp"
+                      src="https://images.unsplash.com/photo-1513483460609-1c8a505ea990?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
+                    />
+                  </StyledBadge>
+                }
+                anchorOriginBlockVertical="bottom"
+                anchorOriginBlockHorizontal="center"
+                anchorPopupBlockVertical="top"
+                anchorPopupBlockHorizontal="center"
               >
-                <Avatar
-                  className={classes.avatar}
-                  alt="Remy Sharp"
-                  src="https://images.unsplash.com/photo-1513483460609-1c8a505ea990?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
-                />
-              </StyledBadge>
+                <Paper className={classes.submenuContainer}>
+                  <Box className={classes.avatarMenuHeader}>
+                    <StyledBadge
+                      overlap="circle"
+                      anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "right",
+                      }}
+                      variant="dot"
+                    >
+                      <Avatar src="https://images.unsplash.com/photo-1513483460609-1c8a505ea990?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80" />
+                    </StyledBadge>
+                    <Typography variant="subtitle2">Ivan Ivanovich</Typography>
+                    <Typography variant="caption"> Active</Typography>
+                  </Box>
+                  <Divider />
+                  <MenuItem>
+                    <Typography variant="body1">
+                      Set yourself as <b>away</b>
+                    </Typography>
+                  </MenuItem>
+                  <Divider />
+                  <EditProfileModal opener={
+                  <MenuItem>
+                    <Typography variant="body1">Edit profile</Typography>
+                  </MenuItem>} />
+                  <MenuItem>
+                    <Typography variant="body1">View profile</Typography>
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem>
+                    <Typography variant="body1">
+                      Sign out of Test company
+                    </Typography>
+                  </MenuItem>
+                </Paper>
+              </SimplePopover>
             </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
-      <Grid container wrap="nowrap">
+      <Grid container wrap="nowrap" className={classes.companyContentWrapper}>
         <Grid item xs={3} className={classes.channelSidebar}>
           <CompanyMenuButton
             buttonClassName={classes.companyMenuButton}
@@ -226,43 +306,72 @@ export const Company = () => {
               </Typography>
             </Grid>
             <Divider />
-            <MenuItem>Invite people to Test Company**</MenuItem>
-            <MenuItem>Create a channel</MenuItem>
+            <MenuItem>
+              <Typography variant="body1">
+                Invite people to Test Company**
+              </Typography>
+            </MenuItem>
+            <MenuItem>
+              <Typography variant="body1">Create a channel</Typography>
+            </MenuItem>
             <Divider />
             <SimplePopover
-              itemChildren="Administration"
+              opener={
+                <MenuItem aria-describedby="Administation">
+                  <Typography variant="body1">Administration</Typography>
+                  <ArrowForwardIosIcon fontSize="small" />
+                </MenuItem>
+              }
               anchorOriginBlockVertical="center"
               anchorOriginBlockHorizontal="right"
               anchorPopupBlockVertical="center"
               anchorPopupBlockHorizontal="center"
-              arrow
             >
               <Paper className={classes.submenuContainer}>
                 <Typography className={classes.captionText} variant="caption">
                   Administration
                 </Typography>
-                <MenuItem>Manage members</MenuItem>
-                <MenuItem>Manage apps</MenuItem>
+                <MenuItem>
+                  <Typography variant="body1">Manage members</Typography>
+                </MenuItem>
+                <MenuItem>
+                  <Typography variant="body1">Manage apps</Typography>
+                </MenuItem>
               </Paper>
             </SimplePopover>
-            <MenuItem>Sign out of Test company</MenuItem>
+            <MenuItem>
+              <Typography variant="body1">Sign out of Test company</Typography>
+            </MenuItem>
           </CompanyMenuButton>
-          <List>
-            {["Mentions & reactions", "Saved items", "More"].map((item) => (
-              <SidebarListCreator componentName={item} />
-            ))}
-            <MoreMenu />
-          </List>
-          <NestedList buttonText="Add channel" listTitle="Channels">
-            {["general", "project"].map((item) => (
-              <Channel name={item} />
-            ))}
-          </NestedList>
-          <NestedList listTitle="Direct messages" buttonText="Add teammate">
-            {["Ivan", "Bob"].map((user) => (
-              <DirectMessageListItem name={user} />
-            ))}
-          </NestedList>
+          <Box className={classes.channelSideBarItemsWrapper}>
+            <List>
+              {["Mentions & reactions", "Saved items", "More"].map((item) => (
+                <SidebarListCreator componentName={item} />
+              ))}
+              <MoreMenu />
+            </List>
+            <NestedList buttonText="Add channel" listTitle="Channels">
+              {["general", "project"].map((item) => (
+                <Channel name={item} />
+              ))}
+            </NestedList>
+            <NestedList listTitle="Direct messages" buttonText="Add teammate">
+              {[
+                "Ivan",
+                "Bob",
+                "Jerry",
+                "Alex",
+                "Some",
+                "Ivan",
+                "Bob",
+                "Jerry",
+                "Alex",
+                "Some",
+              ].map((user) => (
+                <DirectMessageListItem name={user} />
+              ))}
+            </NestedList>
+          </Box>
         </Grid>
         <Grid item xs className={classes.workspace}>
           <Grid
@@ -329,7 +438,7 @@ export const Company = () => {
               className={classes.workspaceContentMessages}
               direction="column"
               wrap="nowrap"
-            > 
+            >
               <MessagePane
                 header="This space is just for you"
                 icon={<QuestionAnswerOutlinedIcon />}
@@ -344,10 +453,11 @@ export const Company = () => {
                 })
                 .map((item) => (
                   <MessagePane header={item.header} avatarSrc={item.avatarSrc}>
-                    <Typography className={classes.message}>MessageMessageMessageMessageMessageMessageMessage
-                    MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage
-                  </Typography>
-                    </MessagePane>
+                    <Typography className={classes.message}>
+                      MessageMessageMessageMessageMessageMessageMessage
+                      MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage
+                    </Typography>
+                  </MessagePane>
                 ))}
               <MessagePane
                 header="Ivan"
@@ -365,8 +475,8 @@ export const Company = () => {
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={3}>
-          <RightSideBlock type="profile"/>
+        <Grid className={classes.rightSideInfoWrapper} item xs={3}>
+          <RightSideBlock type="profile" />
         </Grid>
       </Grid>
     </div>
