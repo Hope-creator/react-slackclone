58bf21f0abe3ed17f7857feeb88345be712886e9
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-27 00:00:31 +0400

    Create store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+
 
 import ReactDOM from "react-dom";
 
@@ -8,12 +10,15 @@ import "./index.css";
 import { theme } from "./theme";
 
 import App from "./App";
+import { store } from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+      <Provider store={store}>
         <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
