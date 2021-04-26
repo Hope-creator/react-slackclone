3a009fd3fd70a45eb08e82cfccc1222a24e27956
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-27 00:00:20 +0400

    Refactoring for flex usage

--- src/components/RightSideBlock/index.tsx
+++ src/components/RightSideBlock/index.tsx
@@ -1,25 +1,48 @@
 import { Box } from "@material-ui/core";
 import React from "react";
+import { IUser } from "../../store/modules/user/types";
 import { Accordeons } from "./Accordions/Accordions";
 import { Files } from "./Accordions/Files";
 import { ContentButtons } from "./ContentButtons";
 import Header from "./Header";
+import ProfileInfo from "./ProfileInfo";
 
 interface RightSideBlockProps {
   headerSubText?: string;
   type: string;
+  content: IUser;
 }
 
 export const RightSideBlock: React.FC<RightSideBlockProps> = ({
   type,
-  headerSubText
+  headerSubText,
+  content
 }) => {
-  return (
-    <Box>
-      <Header />
+
+  if(type === 'channel') {
+    return (
+      <Box>
+      <Header headerTitle="Details" />
       <ContentButtons />
       <Accordeons />
       <Files />
     </Box>
-  );
+    )
+  }
+  if(type === 'profile'){
+
+  const profile = {
+    
+  }
+
+  return (
+    <Box>
+    <Header headerTitle="Profile" />
+    <ProfileInfo
+    
+    />
+  </Box>
+  );}
+
+  return null
 };
