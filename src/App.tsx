4c99917e926642957230dceb9bbf409a6cd8daa5
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-26 23:59:53 +0400

    Create fetch user

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import { GetStarted } from "./pages/GetStarted";
 import { SignIn } from "./pages/SignIn";
 import { Route, Switch } from "react-router";
 import { Company } from "./pages/Company";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "./store/modules/user/user";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
   return (
     <div>
       <Switch>
