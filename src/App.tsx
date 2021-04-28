1b8d0cd865f6a98af06bff81aacdbecca4bb67b2
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-29 00:44:59 +0400

    Add user selector

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import { GetStarted } from "./pages/GetStarted";
 import { SignIn } from "./pages/SignIn";
 import { Route, Switch } from "react-router";
 import { Company } from "./pages/Company";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "./store/modules/user/user";
+import { selectIsUserLoaded } from "./store/modules/user/selectors";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 function App() {
-
   const dispatch = useDispatch();
 
   React.useEffect(() => {
     dispatch(fetchUser());
   }, [dispatch]);
+
+  const isUserLoaded = useSelector(selectIsUserLoaded);
+
+  if (!isUserLoaded) return <CircularProgress disableShrink />;
 
   return (
     <div>
