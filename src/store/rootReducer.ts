f5f380a1a81464ab3cd684ab48c8633700bc73f0
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-29 00:47:09 +0400

    Add company reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
+import company from './modules/company/company';
 import user from './modules/user/user';
 
 export const rootReducer = combineReducers({
-    user: user
+    user: user,
+    company: company
 })
