045c2afcb3c1669c104fdb0a891c886bb4c43646
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-29 00:45:14 +0400

    Refactoring

--- src/components/RightSideBlock/ProfileInfo.tsx
+++ src/components/RightSideBlock/ProfileInfo.tsx
@@ -1,90 +1,87 @@
 import React from "react";
-import defaultAvatar from "../../images/defaulAvatar.png";
+import defaultAvatar from "../../images/defaultAvatar.png";
 
 import { Avatar, Box } from "@material-ui/core";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
-import Header from "./Header";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import CreateIcon from "@material-ui/icons/Create";
 import IconButton from "@material-ui/core/IconButton";
 import CommentOutlinedIcon from "@material-ui/icons/CommentOutlined";
+import { IUser } from "../../store/modules/user/types";
 
 interface IProfileInfoProps {
-  avatarSrc: string;
-  name: string;
-  title?: string;
-  displayName?: string;
-  phone?: number;
-  email: string;
-  self: boolean;
+  user: IUser;
   handleClick?: () => void;
 }
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    headerContainer: {
-      width: "100%",
-      height: "64px",
-      borderBottom: "1px solid rgb(233,227,230)",
-      padding: "0 16px",
+    profileName: {
+      textAlign: "center"
     },
-    headerTitle: {
-      fontWeight: 700,
-    },
+    profileField: {
+      padding: "8px 16px"
+    }
   })
 );
 
 export const ProfileInfo: React.FC<IProfileInfoProps> = ({
-  avatarSrc = defaultAvatar,
-  name,
-  title,
-  displayName,
-  phone,
-  email,
-  self,
+  user,
   handleClick,
 }) => {
   const classes = useStyles();
 
   return (
     <Box>
-      <Header headerTitle="Profile" />
-      <Avatar src={avatarSrc} alt={`profile_${name}_image_${avatarSrc}`} />
+      <Avatar
+        style={{
+          height: "256px",
+          width: "256px",
+          borderRadius: "4px",
+          margin: "16px auto",
+        }}
+        src={user.avatar ? user.avatar : defaultAvatar}
+        alt={`profile_${user.name}_image_${user.avatar}`}
+      />
       <Box>
-        <Typography variant="h5">{name}</Typography>
-        {title ? (
-          <Typography>{title}</Typography>
-        ) : self ? (
-          <Link>Add a title</Link>
-        ) : null}
-        {self ? (
-          <IconButton>
-            <CreateIcon />
-            Edit profile
-          </IconButton>
-        ) : (
-          <IconButton>
-            <CommentOutlinedIcon />
-            Message
-          </IconButton>
-        )}
+        <Box className={classes.profileName}>
+          <Typography variant="h6" color="textPrimary">
+            {user.real_name}
+          </Typography>
+          {user.work ? (
+            <Typography>{user.work}</Typography>
+          ) : user.work ? (
+            <Link>Add a title</Link>
+          ) : null}
+          {user.is_admin ? (
+            <IconButton>
+              <CreateIcon />
+              Edit profile
+            </IconButton>
+          ) : (
+            <IconButton>
+              <CommentOutlinedIcon />
+              Message
+            </IconButton>
+          )}
+        </Box>
         <Box>
-          {displayName ? (
-            <Box>
-              <Typography variant="subtitle1">Display name</Typography>
-              <Typography>{displayName}</Typography>
+          {user.name ? (
+            <Box className={classes.profileField}>
+              <Typography variant="caption">Display name</Typography>
+              <Typography>{user.name}</Typography>
             </Box>
           ) : null}
-          {phone ? (
-            <Box>
-              <Typography variant="subtitle1">Display name</Typography>
-              <Typography>{phone}</Typography>
+          {user.phone ? (
+            <Box className={classes.profileField}>
+              <Typography variant="caption">Phone number</Typography>
+              <Typography>{user.phone}</Typography>
             </Box>
           ) : null}
-          <Box>
-            <Typography variant="subtitle1">Email address</Typography>
-            <Typography>{email}</Typography>
+          <Box className={classes.profileField}>
+            <Typography variant="caption">Email address</Typography>
+            <Typography>{user.email}</Typography>
           </Box>
         </Box>
       </Box>
