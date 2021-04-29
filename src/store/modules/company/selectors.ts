0fa7fd07c2dd18a8ab19dfc75acd243ad9834a1a
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-29 23:24:48 +0400

    Refactor company selectors

--- src/store/modules/company/selectors.ts
+++ src/store/modules/company/selectors.ts
@@ -2,6 +2,8 @@ import { IRootState } from "../../store";
 
 export const selectCompanyState = (state: IRootState) => state.company;
 
-export const selectCompany = (state: IRootState) => selectCompanyState(state).company;
+export const selectCompany = (state: IRootState) =>
+  selectCompanyState(state).company;
 
-export const selectCompanyLoadingState = (state: IRootState) =>selectCompanyState(state).loadingState;
+export const selectCompanyLoadingState = (state: IRootState) =>
+  selectCompanyState(state).loadingState;
