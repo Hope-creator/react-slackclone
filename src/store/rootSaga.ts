06e58615473a8ecd7e14f85bad98823874e1f9e1
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-29 23:24:27 +0400

    Add company to root

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
+import { companySaga } from "./modules/company/operations";
 import { userSaga } from "./modules/user/operations";
 
 export default function* rootSaga() {
-  yield all([userSaga()]);
+  yield all([userSaga(), companySaga()]);
 }
