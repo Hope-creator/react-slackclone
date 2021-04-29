1a0c513c9eaa4923b358aada4c59083e9ca6a90d
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-29 23:29:46 +0400

    Add company saga functional

--- src/store/modules/company/operations.ts
+++ src/store/modules/company/operations.ts
@@ -1,17 +1,20 @@
-import { takeEvery, call, put } from "redux-saga/effects";
+import { takeEvery, call, put, select } from "redux-saga/effects";
 import { ICompany, LoadingCompanyState } from "./types";
 import { fetchCompany, setCompany, setCompanyLoadingState } from "./company";
 import { companyApi } from "../../../services/api/companyApi";
+import { selectUser } from "../user/selectors";
+import { IUser } from "../user/types";
 
 function* fetchCompanySaga() {
   try {
-    const company: ICompany = yield call(companyApi.fetchCompany);
+    const user: IUser = yield select(selectUser);
+    const company: ICompany = yield call(companyApi.fetchCompany, user.companyId);
     yield put(setCompany(company));
   } catch (e) {
     yield put(setCompanyLoadingState(LoadingCompanyState.ERROR));
   }
 }
 
-export function* userSaga() {
+export function* companySaga() {
   yield takeEvery(fetchCompany.type, fetchCompanySaga);
 }
