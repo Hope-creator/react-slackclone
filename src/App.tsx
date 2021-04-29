803a6deeee722df06cf4520d1f96e5b626e04391
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-29 23:28:30 +0400

    Refactor user fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { GetStarted } from "./pages/GetStarted";
 import { SignIn } from "./pages/SignIn";
-import { Route, Switch } from "react-router";
+import { Route, Switch, useHistory } from "react-router";
 import { Company } from "./pages/Company";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "./store/modules/user/user";
-import { selectIsUserLoaded } from "./store/modules/user/selectors";
+import { selectIsUserLoaded, selectUser } from "./store/modules/user/selectors";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 function App() {
   const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const isUserLoaded = useSelector(selectIsUserLoaded);
+  const history = useHistory();
 
   React.useEffect(() => {
     dispatch(fetchUser());
   }, [dispatch]);
 
-  const isUserLoaded = useSelector(selectIsUserLoaded);
+  React.useEffect(() => {
+    if (user && user.companyId) {
+      history.push(`/${user.companyId}`);
+    }
+  }, [user, history]);
 
   if (!isUserLoaded) return <CircularProgress disableShrink />;
 
@@ -24,7 +31,7 @@ function App() {
       <Switch>
         <Route path="/signin" component={SignIn} />
         <Route path="/get-started" component={GetStarted} />
-        <Route path="/" component={Company} />
+        <Route path="/:teamId" component={Company} />
       </Switch>
     </div>
   );
