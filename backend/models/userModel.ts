42abf6781a04538bef473a991398f582dffbd7ad
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-01 23:59:41 +0400

    Create user model

---
+++ backend/models/userModel.ts
@@ -0,0 +1,45 @@
+import { Schema, Document } from "mongoose";
+import { mongoose } from "../core/db";
+
+export interface IUser extends Document {
+  companyId: string,
+  name: string,
+  email: string,
+  status: string,
+  isAdmin: boolean,
+  password: string,
+  displayName?: string,
+  avatar?: string
+}
+
+const UserSchema = new Schema({
+  companyId: {
+    type: String,
+    default: "T01TE7T5WEV",
+  },
+  name: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    unique: true,
+    type: String,
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+  isAdmin: {
+    type: Boolean,
+    default: true,
+  },
+  password: {
+    require: true,
+    type: String,
+  },
+  displayName: String,
+  avatar: String,
+});
+
+export const UserModel = mongoose.model<IUser>("User", UserSchema);
