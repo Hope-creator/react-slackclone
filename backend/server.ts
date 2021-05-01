06e1d709c3233709c30c2e95b0f27f4e295b550d
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-01 23:58:10 +0400

    Add routres for user and register

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,25 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { UserCtrl } from "./controllers/UserController";
+import { registerValidators } from "./validations/register";
+import { AuthCtrl } from "./controllers/AuthController";
 
 const app = express();
 const port = 5000;
 
-app.get('/hello', (req: express.Request, res: express.Response) => {
-    res.send("hello")
-})
+app.use(express.json());
 
-app.listen(port, ()=> {
-    console.log(`Server is up on ${port}`)
-})
+app.get("/api/users", UserCtrl.index);
+app.post("/api/auth/register", registerValidators, AuthCtrl.create);
+app.get("/api/auth/verification/verify-account/:userId/:secretCode", AuthCtrl.verify);
+//app.put('/users', UserCtrl.update)
+//app.delete('/users', UserCtrl.delete)
+
+//# mongoDB
+import "./core/db";
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
