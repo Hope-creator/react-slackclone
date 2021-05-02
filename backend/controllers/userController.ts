c87c94ce2435235f80c65ecce2168a3780618a9e
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-02 22:12:24 +0400

    uController: add show method

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { isValidObjectId } from "mongoose";
 import { UserModel } from "../models/UserModel";
 
 class UserController {
@@ -9,6 +10,31 @@ class UserController {
         status: "success",
         data: users,
       });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        errors: JSON.stringify(error),
+      });
+      console.log("Error on UserController / index:", error);
+    }
+  }
+
+  async show(req: express.Request, res: express.Response): Promise<void> {
+    try {
+      const userId = req.params.id;
+      if (!isValidObjectId(userId)) {
+        res.status(400).json({ status: "error", message: "Wrong type of ID" });
+      } else {
+        const user = await UserModel.findById(userId).exec();
+        if (!user) {
+          res.status(404).json({ status: "error", message: "User not found" });
+        } else {
+          res.json({
+            status: "success",
+            data: user,
+          });
+        }
+      }
     } catch (error) {
       res.status(500).json({
         status: "error",
