91afe743edbaf86a28bbed0741cc2fb9f24c89b9
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-02 22:13:28 +0400

    company: Create company model

---
+++ backend/models/CompanyModel.ts
@@ -0,0 +1,38 @@
+import { IUser } from './../../src/store/modules/user/types';
+import { Schema, Document } from "mongoose";
+import { mongoose } from "../core/db";
+
+export interface ICompany extends Document {
+  _id: string,
+  name: string,
+  primary_owner: {
+    user_id: string,
+    email: string,
+  },
+  members: IUser[] | never[]
+}
+
+const CompanySchema = new Schema({
+  _id: {
+    type: String,
+    default: "T01TE7T5WEV"
+  },
+  name: {
+    required: true,
+    type: String,
+    default: "My new company"
+  },
+  primary_owner: {
+    user_id: {
+      type: String,
+      default: "608e5c3854920a1b14202ab5"
+    },
+    email: {
+      type: String,
+      default: "goryachkin.ivan1@mail.ru"
+    }
+  },
+  members: []
+}, {versionKey: false});
+
+export const CompanyModel = mongoose.model<ICompany>("Company", CompanySchema);
