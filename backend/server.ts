6ae4f9667588d6128ec5d27825f5778b16d7eb87
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-02 22:14:00 +0400

    server: Add coockie session and roots

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,4 @@
+import { authencticateToken } from './utils/middleware/checkAuth';
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -11,14 +12,31 @@ const port = 5000;
 
 app.use(express.json());
 
-app.get("/api/users", UserCtrl.index);
-app.post("/api/auth/register", registerValidators, AuthCtrl.create);
-app.get("/api/auth/verification/verify-account/:userId/:secretCode", AuthCtrl.verify);
-//app.put('/users', UserCtrl.update)
-//app.delete('/users', UserCtrl.delete)
+// #Cookie session
+import coockieSession from 'cookie-session'
+app.use(coockieSession({
+  secret: process.env.COOKIE_SESSION_SECRET,
+  maxAge: 1000 * 60 * 60 * 24 * 14,
+  httpOnly: true,
+  secure: false,
+}))
+
+
 
 //# mongoDB
 import "./core/db";
+import { loginValidators } from './validations/login';
+import { CompanyCtrl } from './controllers/CompanyController';
+
+// # Routes
+app.get("/api/users", UserCtrl.index);
+app.get("/api/users/:id",authencticateToken, UserCtrl.show);
+app.get("/api/company/:id",authencticateToken, CompanyCtrl.show);
+app.post("/api/auth/login",loginValidators, AuthCtrl.login)
+app.post("/api/auth/register", registerValidators, AuthCtrl.create);
+app.get("/api/auth/verification/verify-account/:userId/:secretCode", authencticateToken, AuthCtrl.verify);
+//app.put('/users', UserCtrl.update)
+//app.delete('/users', UserCtrl.delete)
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
