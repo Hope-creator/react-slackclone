e47c3874aa0700782c0ea345a08f9e1f2e8a6779
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-02 22:13:41 +0400

    company: Create company controller

---
+++ backend/controllers/CompanyController.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { CompanyModel } from "../models/CompanyModel";
+
+class CompanyController {
+  async show(req: express.Request, res: express.Response): Promise<void> {
+    try {
+      const companyId = req.params.id;
+      const company = await CompanyModel.findById(companyId).exec();
+      if (!company) {
+        res.status(404).json({ status: "error", message: "Company not found" });
+      } else {
+        res.json({
+          status: "success",
+          data: company,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        errors: JSON.stringify(error),
+      });
+      console.log("Error on CompanyController / show:", error);
+    }
+  }
+}
+
+export const CompanyCtrl = new CompanyController();
