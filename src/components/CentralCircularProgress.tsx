04322d648e38a69e994d3091dbbafadd8521532b
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-07 00:30:25 +0400

    Create component

---
+++ src/components/CentralCircularProgress.tsx
@@ -0,0 +1,12 @@
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { CircularProgressProps } from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
+import React from 'react';
+
+export const CentralCircularProgress = withStyles({
+    root: {
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+    },
+  })((props: CircularProgressProps) => <CircularProgress {...props} />);
