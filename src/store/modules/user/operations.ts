690b9d8afa1aeb2033ac952882933a35d767066f
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-07 00:33:50 +0400

    Refactor user operations

--- src/store/modules/user/operations.ts
+++ src/store/modules/user/operations.ts
@@ -1,18 +1,33 @@
-import { userApi } from "./../../../services/api/userApi";
+import { ILoginForm } from "./../../../components/SignInForm";
 import { takeEvery, call, put } from "redux-saga/effects";
 import { IUser, LoadingUserState } from "./types";
-import { fetchUser, setUser, setUserLoadingState } from "./user";
+import { fetchMe, fetchUser, setUser, setUserLoadingState } from "./user";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { authApi } from "../../../services/api/authApi";
 
-function* fetchUserSaga() {
-    try{
-        const user: IUser = yield call(userApi.fetchUser);
-        yield put(setUser(user))
+function* fetchMeSaga() {
+  try {
+    const user: IUser = yield call(authApi.getMe);
+    yield put(setUser(user));
+  } catch (e) {
+    if (e.message.includes("401")) {
+      yield put(setUserLoadingState(LoadingUserState.LOADED));
+    } else {
+      yield put(setUserLoadingState(LoadingUserState.ERROR));
     }
-    catch(e) {
-        yield put(setUserLoadingState(LoadingUserState.ERROR))
-    }
+  }
+}
+
+function* fetchUserSaga(action: PayloadAction<ILoginForm>) {
+  try {
+    const user: IUser = yield call(authApi.signIn, action.payload);
+    yield put(setUser(user));
+  } catch (e) {
+    yield put(setUserLoadingState(LoadingUserState.ERROR));
+  }
 }
 
 export function* userSaga() {
   yield takeEvery(fetchUser.type, fetchUserSaga);
+  yield takeEvery(fetchMe.type, fetchMeSaga);
 }
