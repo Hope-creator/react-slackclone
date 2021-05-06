916a7479ff4c6040876af92c82219390f24c0131
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-07 00:35:01 +0400

    Refactor user stateloading selector

--- src/store/modules/user/selectors.ts
+++ src/store/modules/user/selectors.ts
@@ -1,15 +1,7 @@
-import { LoadingUserState } from "./types";
 import { IRootState } from "../../store";
 
 export const selectUserState = (state: IRootState) => state.user;
 
 export const selectUser = (state: IRootState) => selectUserState(state).user;
 
-export const selectUserLoading = (state: IRootState) =>
-  selectUserState(state).loadingState === LoadingUserState.LOADING;
-
-export const selectIsUserLoaded = (state: IRootState) =>
-  selectUserState(state).loadingState === LoadingUserState.LOADED;
-
-export const selectIsUserNeverLoaded = (state: IRootState) =>
-  selectUserState(state).loadingState === LoadingUserState.NEVER;
+export const selectUserLoadingState = (state: IRootState) => selectUserState(state).loadingState;
