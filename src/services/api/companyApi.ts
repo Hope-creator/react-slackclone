418381e5a963801b6d5f8dc3c09b67f87664bcc6
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-07 00:24:45 +0400

    Delete company functional

--- src/services/api/companyApi.ts
+++
@@ -1,9 +0,0 @@
-import { ICompany } from './../../store/modules/company/types';
-import axios from "axios"; 
-
-export const companyApi = {
-    async fetchCompany (companyId: number): Promise<ICompany> {
-        const response = await axios.get(`http://localhost:5000/${companyId}`);
-        return response.data.company
-    }
-}
--- src/store/modules/company/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { IRootState } from "../../store";
-
-export const selectCompanyState = (state: IRootState) => state.company;
-
-export const selectCompany = (state: IRootState) =>
-  selectCompanyState(state).company;
-
-export const selectCompanyLoadingState = (state: IRootState) =>
-  selectCompanyState(state).loadingState;
