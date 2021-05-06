2b2ab731de8c3a28920bf15b862371bf28e621f0
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-07 00:24:58 +0400

    Add response interface

--- src/services/api/userApi.ts
+++ src/services/api/userApi.ts
@@ -1,9 +1,14 @@
 import { IUser } from './../../store/modules/user/types';
 import axios from "axios"; 
 
+export interface IResponse<T> {
+    status: string,
+    data: T
+}
+
 export const userApi = {
-    async fetchUser (): Promise<IUser> {
-        const response = await axios.get('http://localhost:5000/user');
-        return response.data
+    async fetchUser (id: string): Promise<IUser> {
+        const response = await axios.get<IResponse<IUser>>(`/api/users/${id}`);
+        return response.data.data
     }
 }
