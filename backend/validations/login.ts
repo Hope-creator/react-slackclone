d672a58dec20efebf33137deec205a659d715a25
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-07 00:29:08 +0400

    Delete password validation

--- backend/validations/login.ts
+++ backend/validations/login.ts
@@ -2,7 +2,4 @@ import { body } from "express-validator";
 
 export const loginValidators = [
   body("email").isEmail().withMessage("Wrong type of email"),
-  body("password")
-    .isLength({ min: 6 })
-    .withMessage("Minimum password lenght is 6 characters")
 ];
--- backend/validations/register.ts
+++ backend/validations/register.ts
@@ -10,8 +10,6 @@ export const registerValidators = [
     })
     .withMessage("Max length of name is from 2 to 40 characters"),
   body("password")
-    .isLength({ min: 6 })
-    .withMessage("Minimum password lenght is 6 characters")
     .custom((value, { req }) => {
       if (value !== req.body.password2) {
         throw new Error("Passwords do not match");
