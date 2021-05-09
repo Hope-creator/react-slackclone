02a6b09759954d8db9f6668c9fc6da1582c60432
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-10 00:03:23 +0400

    Add conversations and messages to rootSaga

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,6 +1,8 @@
 import { all } from "redux-saga/effects";
+import { conversationsSaga } from "./modules/conversations/operations";
+import { messagesSaga } from "./modules/messages/operations";
 import { userSaga } from "./modules/user/operations";
 
 export default function* rootSaga() {
-  yield all([userSaga()]);
+  yield all([userSaga(), messagesSaga(), conversationsSaga()]);
 }
