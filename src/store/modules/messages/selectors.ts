121835bf01cf53a947b2d9e2d5a55d00c7f93df4
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-10 00:06:18 +0400

    Create / messages selectors

---
+++ src/store/modules/messages/selectors.ts
@@ -0,0 +1,9 @@
+import { IRootState } from "../../store";
+
+export const selectMessagesState = (state: IRootState) => state.messages;
+
+export const selectMessages = (state: IRootState) =>
+  selectMessagesState(state).messages;
+
+export const selectMessagesLoadingState = (state: IRootState) =>
+  selectMessagesState(state).loadingState;
