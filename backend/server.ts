d67e7f426104c55cd545ab842317b27fcce42f29
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-09 23:55:37 +0400

    Refactoring / sockets and routes

--- backend/server.ts
+++ backend/server.ts
@@ -1,44 +1,37 @@
-import { authencticateToken } from './utils/middleware/checkAuth';
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import { createServer } from "http";
+import createSocket from "./core/socket";
+import createRoutes from "./core/routes";
 
-import { UserCtrl } from "./controllers/UserController";
-import { registerValidators } from "./validations/register";
-import { AuthCtrl } from "./controllers/AuthController";
+dotenv.config();
 
 const app = express();
 const port = 5000;
 
+const http = createServer(app);
+const io = createSocket(http);
+
 app.use(express.json());
 
 // #Cookie session
-import coockieSession from 'cookie-session'
-app.use(coockieSession({
-  secret: process.env.COOKIE_SESSION_SECRET,
-  maxAge: 1000 * 60 * 60 * 24 * 14,
-  httpOnly: true,
-  secure: false,
-}))
+import coockieSession from "cookie-session";
+app.use(
+  coockieSession({
+    secret: process.env.COOKIE_SESSION_SECRET,
+    maxAge: 1000 * 60 * 60 * 24 * 14,
+    httpOnly: true,
+    secure: false,
+  })
+);
 
 
+
+createRoutes(app, io);
 
 //# mongoDB
 import "./core/db";
-import { loginValidators } from './validations/login';
-import { CompanyCtrl } from './controllers/CompanyController';
 
-// # Routes
-app.get("/api/users", UserCtrl.index);
-app.get("/api/users/:id",authencticateToken, UserCtrl.show);
-app.get("/api/company/:id",authencticateToken, CompanyCtrl.show);
-app.get("/api/auth/me",authencticateToken, AuthCtrl.getMe)
-app.post("/api/auth/login",loginValidators, AuthCtrl.login)
-app.post("/api/auth/register", registerValidators, AuthCtrl.create);
-app.get("/api/auth/verification/verify-account/:userId/:secretCode", authencticateToken, AuthCtrl.verify);
-//app.put('/users', UserCtrl.update)
-//app.delete('/users', UserCtrl.delete)
-
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
