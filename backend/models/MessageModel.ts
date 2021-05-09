9c7e0bb8132b9a60e9ca7ed4eae62ded93fd3552
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-09 23:59:32 +0400

    Create message model

---
+++ backend/models/MessageModel.ts
@@ -0,0 +1,34 @@
+import { IConversation } from './ConversationModel';
+import { Schema, Document } from "mongoose";
+import { mongoose } from "../core/db";
+import { IUser } from './UserModel';
+
+export interface IMessage {
+  creator: IUser | string;
+  dest: IConversation | string;
+  text: string;
+  read: boolean;
+}
+
+export interface IMessageDocument extends Document {}
+
+const MessageSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  dest: {
+    type: Schema.Types.ObjectId,
+    ref: 'Conversation'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+}, {versionKey: false, timestamps: true});
+
+export const MessageModel = mongoose.model<IMessageDocument>("Message", MessageSchema);
