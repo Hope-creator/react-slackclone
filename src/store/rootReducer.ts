7df82bb949a039bbfe0b2cfbccac85dac6a2a60e
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-10 00:03:11 +0400

    Add conversations and messages to root reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,10 @@
 import {combineReducers} from 'redux';
+import conversations from './modules/conversations/conversations';
+import messages from './modules/messages/messages';
 import user from './modules/user/user';
 
 export const rootReducer = combineReducers({
-    user: user
+    user: user,
+    conversations: conversations,
+    messages: messages
 })
