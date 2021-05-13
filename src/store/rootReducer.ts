8f16dd1f9c6cfa21458f10de83ff665cf16a0bb6
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-13 22:22:24 +0400

    Add current conversations and current info to rooReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import conversations from './modules/conversations/conversations';
-import messages from './modules/messages/messages';
+import currentConversation from './modules/currentConversation/currentConversation';
+import currentInfo from './modules/currentInfo/currentInfo';
 import user from './modules/user/user';
 
 export const rootReducer = combineReducers({
     user: user,
     conversations: conversations,
-    messages: messages
+    currentConversation: currentConversation,
+    currentInfo: currentInfo
 })
