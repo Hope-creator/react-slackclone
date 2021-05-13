b0d56b205674924eba22a4066e486289e91a34da
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-13 22:23:01 +0400

    Create current conversation selectors

---
+++ src/store/modules/currentConversation/selectors.ts
@@ -0,0 +1,13 @@
+import { IRootState } from "../../store";
+
+export const selectCurrentConversationState = (state: IRootState) =>
+  state.currentConversation;
+
+export const selectCurrentConversation = (state: IRootState) =>
+  selectCurrentConversationState(state).currentConversation;
+
+export const selectMessages = (state: IRootState) =>
+  selectCurrentConversationState(state).messages;
+
+export const selectCurrentConversationLoadingState = (state: IRootState) =>
+  selectCurrentConversationState(state).loadingState;
