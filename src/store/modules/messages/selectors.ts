b3090295beb31654832f6671f1c4cb8ff1c2932d
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-13 22:20:49 +0400

    Delete messages modules

--- src/store/modules/messages/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { IRootState } from "../../store";
-
-export const selectMessagesState = (state: IRootState) => state.messages;
-
-export const selectMessages = (state: IRootState) =>
-  selectMessagesState(state).messages;
-
-export const selectMessagesLoadingState = (state: IRootState) =>
-  selectMessagesState(state).loadingState;
