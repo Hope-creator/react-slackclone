8cf2d43328fee4ff59629014b655e44daa81b15c
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-13 22:19:58 +0400

    Fix folders in message api

--- src/services/api/messagesApi.ts
+++ src/services/api/messagesApi.ts
@@ -1,10 +1,10 @@
 import axios from "axios"; 
-import { IMessage } from "../../store/modules/messages/types";
+import { IMessage } from "../../store/modules/currentConversation/types";
 import { IResponse } from "./types";
 
 export const messagesApi = {
     async fetchMessages (id: string): Promise<IMessage[] | []> {
-        const response = await axios.get<IResponse<IMessage[] | []>>(`/api/conversations/${id}`);
+        const response = await axios.get<IResponse<IMessage[] | []>>(`/api/messages/${id}`);
         return response.data.data
     },
 }
