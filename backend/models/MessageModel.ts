cac74ac2029e454c1e5439323f98651ef430726e
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-13 21:45:59 +0400

    Add marked to message model

--- backend/models/MessageModel.ts
+++ backend/models/MessageModel.ts
@@ -1,34 +1,42 @@
-import { IConversation } from './ConversationModel';
+import { IConversation } from "./ConversationModel";
 import { Schema, Document } from "mongoose";
 import { mongoose } from "../core/db";
-import { IUser } from './UserModel';
+import { IUser } from "./UserModel";
 
 export interface IMessage {
   creator: IUser | string;
   dest: IConversation | string;
   text: string;
   read: boolean;
+  marked: boolean;
 }
 
-export interface IMessageDocument extends Document {}
+export interface IMessageDocument extends Document, IMessage {}
 
-const MessageSchema = new Schema({
-  creator: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
+const MessageSchema = new Schema(
+  {
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    dest: {
+      type: Schema.Types.ObjectId,
+      ref: "Conversation",
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    marked: { type: Boolean, default: false },
   },
-  dest: {
-    type: Schema.Types.ObjectId,
-    ref: 'Conversation'
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  read: {
-    type: Boolean,
-    default: false,
-  },
-}, {versionKey: false, timestamps: true});
+  { versionKey: false, timestamps: true }
+);
 
-export const MessageModel = mongoose.model<IMessageDocument>("Message", MessageSchema);
+export const MessageModel = mongoose.model<IMessageDocument>(
+  "Message",
+  MessageSchema
+);
