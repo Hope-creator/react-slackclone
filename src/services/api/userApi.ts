d5d47d975dfaeff25e432bdda120fe3e32f7595d
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-13 22:22:07 +0400

    Change name to getUser in userAPI

--- src/services/api/userApi.ts
+++ src/services/api/userApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { IResponse } from './types';
 
 export const userApi = {
-    async fetchUser (id: string): Promise<IUser> {
+    async getUser (id: string): Promise<IUser> {
         const response = await axios.get<IResponse<IUser>>(`/api/users/${id}`);
         return response.data.data
     }
