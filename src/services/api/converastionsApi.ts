d78f27097ffb1c72d26705eba6e299f9b7110550
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-19 11:03:13 +0400

    Add featch populated conversation and create DM to api

--- src/services/api/converastionsApi.ts
+++ src/services/api/converastionsApi.ts
@@ -11,10 +11,23 @@ export const conversationsApi = {
         const response = await axios.get<IResponse<IConversation>>(`/api/conversations/${id}`);
         return response.data.data
     },
+    async fetchConversationWithPopulate (id: string): Promise<IConversation> {
+        const response = await axios.get<IResponse<IConversation>>(`/api/conversations/populated/${id}`);
+        return response.data.data
+    },
     async createChannel (name: string): Promise<IConversation> {
         const response = await axios.post<IResponse<IConversation>>(`/api/conversations/`, {
             name: name,
             isChannel: true
+        });
+        console.log(response)
+        return response.data.data
+    },
+    async createDirectMessage (name: string, id: string): Promise<IConversation> {
+        const response = await axios.post<IResponse<IConversation>>(`/api/conversations/`, {
+            name: name,
+            isChannel: false,
+            id: id
         });
         console.log(response)
         return response.data.data
