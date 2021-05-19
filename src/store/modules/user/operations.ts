55c3b373293bfde5dbedb8c042f07a450467e8ee
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-19 11:00:03 +0400

    Add create user saga

--- src/store/modules/user/operations.ts
+++ src/store/modules/user/operations.ts
@@ -1,9 +1,10 @@
 import { ILoginForm } from "./../../../components/SignInForm";
 import { takeEvery, call, put } from "redux-saga/effects";
 import { IUser, LoadingUserState } from "./types";
-import { fetchMe, fetchUser, setUser, setUserLoadingState } from "./user";
+import { fetchMe, fetchUser, setUser, setUserLoadingState, createUser } from "./user";
 import { PayloadAction } from "@reduxjs/toolkit";
 import { authApi } from "../../../services/api/authApi";
+import { IGetStartedForm } from "../../../components/GetStartedForm";
 
 function* fetchMeSaga() {
   try {
@@ -27,7 +28,17 @@ function* fetchUserSaga(action: PayloadAction<ILoginForm>) {
   }
 }
 
+function* createUserSaga(action: PayloadAction<IGetStartedForm>) {
+  try {
+    const user: IUser = yield call(authApi.signUp, action.payload);
+    yield put(setUser(user));
+  } catch (e) {
+      yield put(setUserLoadingState(LoadingUserState.ERROR));
+  }
+}
+
 export function* userSaga() {
   yield takeEvery(fetchUser.type, fetchUserSaga);
   yield takeEvery(fetchMe.type, fetchMeSaga);
+  yield takeEvery(createUser.type, createUserSaga);
 }
