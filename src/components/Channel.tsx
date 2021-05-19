57a2f3859ada67fd3da7c13f888b94d191248f61
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-19 10:54:30 +0400

    Add text ellipsis and fix channel component

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -1,9 +1,10 @@
 import { ListItem, ListItemIcon } from "@material-ui/core";
 import React, { useCallback } from "react";
-import TextureIcon from "@material-ui/icons/Texture";
+import LockIcon from "@material-ui/icons/Lock";
 import ListItemText from "@material-ui/core/ListItemText";
 import { IConversation } from "../store/modules/conversations/types";
 import { useHistory } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
 
 interface ChanneProps {
   channel: IConversation;
@@ -12,17 +13,37 @@ interface ChanneProps {
 export const Channel: React.FC<ChanneProps> = ({
   channel,
 }: ChanneProps): React.ReactElement => {
-
   const history = useHistory();
-  const pushChannel = useCallback((id)=> {
-    history.push(`/${id}`)
-  } , [history])
+  const pushChannel = useCallback(
+    (id) => {
+      history.push(`/${id}`);
+    },
+    [history]
+  );
   return (
-    <ListItem dense button onClick={()=> pushChannel(channel._id)}>
-      <ListItemIcon>
-        <TextureIcon color="primary" />
-      </ListItemIcon>
-      <ListItemText primaryTypographyProps={{ color: "primary" }}>
+    <ListItem dense button onClick={() => pushChannel(channel._id)}>
+      {channel.is_private ? (
+        <ListItemIcon>
+          <LockIcon color="primary" />
+        </ListItemIcon>
+      ) : (
+        <ListItemIcon>
+          <Typography color="primary" variant="h6">
+            &nbsp;&nbsp;#
+          </Typography>
+        </ListItemIcon>
+      )}
+      <ListItemText
+        primaryTypographyProps={{
+          color: "primary",
+          style: {
+            width: "200px",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          },
+        }}
+      >
         {channel.name}
       </ListItemText>
     </ListItem>
