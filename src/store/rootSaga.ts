d65c79656332b5bea70c8e999ed6ef8bee9a1983
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-19 11:00:27 +0400

    Add current info to root saga

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,8 +1,14 @@
 import { all } from "redux-saga/effects";
 import { conversationsSaga } from "./modules/conversations/operations";
 import { currentConversationSaga } from "./modules/currentConversation/operations";
+import { currentInfoSaga } from "./modules/currentInfo/operations";
 import { userSaga } from "./modules/user/operations";
 
 export default function* rootSaga() {
-  yield all([userSaga(), currentConversationSaga(), conversationsSaga()]);
+  yield all([
+    userSaga(),
+    currentConversationSaga(),
+    conversationsSaga(),
+    currentInfoSaga(),
+  ]);
 }
