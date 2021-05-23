5d92533a5d061e7c1ec7c52c6dfa90ccf09f88a4
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-24 00:29:26 +0400

    Add members to workspace

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,8 +1,30 @@
+import { Button, Typography } from "@material-ui/core";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
-import { fetchCurrentConversation } from "../../store/modules/currentConversation/currentConversation";
-import { selectCurrentConversation, selectMessages } from "../../store/modules/currentConversation/selectors";
+import {
+  clearCurrentConversation,
+  fetchCurrentConversation,
+} from "../../store/modules/currentConversation/currentConversation";
+import {
+  selectCurrentConversation,
+  selectCurrentConversationLoadingState,
+  selectMessages,
+} from "../../store/modules/currentConversation/selectors";
+import { LoadingCurrentConversationState } from "../../store/modules/currentConversation/types";
+import {
+  clearCurrentMembers,
+  fetchCurrentMembers,
+} from "../../store/modules/currentMembers/currentMembers";
+import {
+  selectCurrentMembers,
+  selectCurrentMembersLoadingState,
+} from "../../store/modules/currentMembers/selectors";
+import { LoadingCurrentMembersState } from "../../store/modules/currentMembers/types";
+import { ConversationContent } from "./Conversation/Content/ConversationContent";
+import { LeftSideConversationContent } from "./Conversation/Header/LeftSideConversationContent";
+import { RightSideConversationContent } from "./Conversation/Header/RightSideConversationContent";
+import { MembersContent } from "./Members/Content/MembersContent";
 import { WorkspaceContent } from "./WorkspaceContent";
 import { WorkspaceHeader } from "./WorkspaceHeader";
 
@@ -12,19 +34,87 @@ export const Workspace = () => {
   const path = history.location.pathname;
   const messages = useSelector(selectMessages);
   const currentConversation = useSelector(selectCurrentConversation);
+  const currentMembers = useSelector(selectCurrentMembers);
+  const currentMembersLoadingState = useSelector(
+    selectCurrentMembersLoadingState
+  );
+  const currentConversationLoadingState = useSelector(
+    selectCurrentConversationLoadingState
+  );
 
   React.useEffect(() => {
-      if(path.length === 25) {
-          const fetchPath = path.split("").slice(1).join("");
-          dispatch(fetchCurrentConversation(fetchPath))
-      }
+    if (path.length === 25) {
+      const fetchPath = path.split("").slice(1).join("");
+      dispatch(fetchCurrentConversation(fetchPath));
+    }
+    return function clearConversation() {
+      dispatch(clearCurrentConversation());
+    };
   }, [path, dispatch]);
 
+  React.useEffect(() => {
+    if (path.match("members")) {
+      dispatch(fetchCurrentMembers());
+    }
+    return function clearMembers() {
+      dispatch(clearCurrentMembers());
+    };
+  }, [path, dispatch]);
+
+  if (
+    currentConversation &&
+    messages &&
+    currentConversationLoadingState === LoadingCurrentConversationState.LOADED
+  ) {
+    return (
+      <>
+        <WorkspaceHeader
+          leftSideContent={
+            <LeftSideConversationContent conversation={currentConversation} />
+          }
+          rightSideContent={
+            <RightSideConversationContent conversation={currentConversation} />
+          }
+        />
+        <WorkspaceContent
+          children={<ConversationContent messages={messages} />}
+        />
+      </>
+    );
+  }
+
+  if (
+    currentMembers &&
+    currentMembersLoadingState === LoadingCurrentMembersState.LOADED
+  ) {
+    return (
+      <>
+        <WorkspaceHeader
+          leftSideContent={
+            <>
+              <Typography variant="h6">People</Typography>
+              <Typography variant="caption">
+                {currentMembers.length} members
+              </Typography>
+            </>
+          }
+          rightSideContent={
+            <>
+              <Button>Invite people</Button>
+            </>
+          }
+        />
+        <WorkspaceContent
+          children={<MembersContent members={currentMembers} />}
+        />
+      </>
+    );
+  }
 
   return (
     <>
-      <WorkspaceHeader conversation={currentConversation} />
-      <WorkspaceContent messages={messages} />
+      <WorkspaceHeader />
+      <WorkspaceContent />
     </>
   );
 };
