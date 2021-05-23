53f2baf38e8a73e5089ed302938527d0e2bcfa13
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-24 00:32:46 +0400

    Add currentMebmer reducer to rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,12 +1,14 @@
 import {combineReducers} from 'redux';
 import conversations from './modules/conversations/conversations';
 import currentConversation from './modules/currentConversation/currentConversation';
-import currentInfo from './modules/currentInfo/currentInfo';
+import currentInfo from './modules/currentInfo_side/currentInfo';
+import currentMembers from './modules/currentMembers/currentMembers';
 import user from './modules/user/user';
 
 export const rootReducer = combineReducers({
     user: user,
     conversations: conversations,
     currentConversation: currentConversation,
-    currentInfo: currentInfo
+    currentInfo: currentInfo,
+    currentMembers: currentMembers
 })
