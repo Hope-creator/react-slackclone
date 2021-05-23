0c4411c4b42a2304751687817f29c316400e90f8
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-24 00:22:23 +0400

    Add current members to root state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,12 @@
-import { IUserState } from './modules/user/types';
+import { IUserState } from "./modules/user/types";
 import { createStore, compose, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./rootReducer";
 import rootSaga from "./rootSaga";
-import { IConversationsState } from './modules/conversations/types';
-import { ICurrentConversationState } from './modules/currentConversation/types';
-import { ICurrentInfoState } from './modules/currentInfo/types';
+import { IConversationsState } from "./modules/conversations/types";
+import { ICurrentConversationState } from "./modules/currentConversation/types";
+import { ICurrentInfoState } from "./modules/currentInfo_side/types";
+import { ICurrentMembersState } from "./modules/currentMembers/types";
 
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -13,10 +14,11 @@ const composeEnhancers =
 const sagaMiddleware = createSagaMiddleware();
 
 export interface IRootState {
-  user: IUserState,
-  conversations: IConversationsState,
-  currentConversation: ICurrentConversationState,
-  currentInfo: ICurrentInfoState
+  user: IUserState;
+  conversations: IConversationsState;
+  currentConversation: ICurrentConversationState;
+  currentInfo: ICurrentInfoState;
+  currentMembers: ICurrentMembersState;
 }
 
 export const store = createStore(
