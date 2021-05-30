411ef8e2d02cbe0ad7da6a160b2ad99dbe765966
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-30 15:55:12 +0400

    Add get undread by id and file upload to messages

--- backend/controllers/MessagesController.ts
+++ backend/controllers/MessagesController.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { isValidObjectId } from "mongoose";
+import { constants } from "node:buffer";
 import socket from "socket.io";
 import { ConversationModel } from "../models/ConversationModel";
 import { MarkedMessageModel } from "../models/MarkedMessageModel";
@@ -24,33 +25,76 @@ class MessagesController {
       try {
         const conversation = await ConversationModel.findOne({
           _id: conversationsId,
-          members: userId,
         }).exec();
         if (!conversation) {
           res
             .status(400)
             .json({ status: "error", data: "Conversations doesn't exist" });
         } else {
-          const query = await MessageModel.find({
-            dest: conversation._id,
-          })
-            .populate("creator")
-            .lean();
-          const messages = await Promise.all(
-            query.map(async (message) => {
-              const isMarked = await MarkedMessageModel.exists({
-                user: userId,
-                markedMessage: message._id,
-              });
-              if (isMarked) {
-                message.marked = true;
-              } else {
-                message.marked = false;
-              }
-              return message;
+          if (
+            (conversation.is_private &&
+              conversation.members.includes(userId)) ||
+            !conversation.is_private
+          ) {
+            const query = await MessageModel.find({
+              dest: conversation._id,
             })
-          );
-          res.json({ status: "success", data: messages });
+              .populate("creator")
+              .populate("attachments")
+              .lean();
+            const messages = await Promise.all(
+              query.map(async (message) => {
+                const isMarked = await MarkedMessageModel.exists({
+                  user: userId,
+                  markedMessage: message._id,
+                });
+                if (isMarked) {
+                  message.marked = true;
+                } else {
+                  message.marked = false;
+                }
+                return message;
+              })
+            );
+            res.json({ status: "success", data: messages });
+          } else {
+            res
+              .status(404)
+              .json({ status: "error", data: "Conversation not founded" });
+          }
+        }
+      } catch (error) {
+        res.status(500).json({
+          status: "error",
+          errors: JSON.stringify(error),
+        });
+        console.log("Error on MessagesController / index:", error);
+      }
+    }
+  };
+
+  getUnreadCountById = async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<void> => {
+    const conversationsId = req.params.id;
+    if (!isValidObjectId(conversationsId)) {
+      res.status(400).json({ status: "error", data: "Wrong type of ID" });
+    } else {
+      try {
+        const conversation = await ConversationModel.exists({
+          _id: conversationsId,
+        });
+        if (!conversation) {
+          res
+            .status(400)
+            .json({ status: "error", data: "Conversations doesn't exist" });
+        } else {
+          const count = await MessageModel.count({
+            dest: conversationsId,
+            unreadBy: req.userId,
+          });
+          res.json({ status: "success", data: count });
         }
       } catch (error) {
         res.status(500).json({
@@ -90,16 +134,25 @@ class MessagesController {
         } else {
           const postData = {
             creator: req.userId,
-            dest: req.params.id,
+            dest: conversation.id,
             text: req.body.text,
-            read: false,
+            unreadBy:
+              (!conversation.is_channel &&
+                conversation.members.filter(
+                  (_userId) => _userId.toString() !== userId.toString()
+                )) ||
+              undefined,
+            attachments: req.body.attachments,
           };
           const message = await (await new MessageModel(postData).save())
             .populate("creator")
+            .populate("attachments")
             .execPopulate();
           this.io
             .to(message.dest.toString())
             .emit("SERVER:NEW_MESSAGE", message);
+          if (!conversation.is_channel)
+            this.io.emit("SERVER:NEW_UNREAD", conversation._id);
           res.json({ status: "success", data: message });
         }
       } catch (error) {
