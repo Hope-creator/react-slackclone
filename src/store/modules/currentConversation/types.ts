97366f689bb4e8ff09347507d97f177728b7085e
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-30 16:11:44 +0400

    Add files to message type and loading state for send message

--- src/store/modules/currentConversation/types.ts
+++ src/store/modules/currentConversation/types.ts
@@ -1,3 +1,4 @@
+import { IFile } from "../../../services/api/uploadApi";
 import { IConversation } from "../conversations/types";
 import { IUser } from "../user/types";
 
@@ -8,10 +9,18 @@ export interface IMessage {
   text: string;
   read: boolean;
   marked: boolean;
+  attachments: IFile[];
+  createdAt: string;
 }
 
-
 export enum LoadingCurrentConversationState {
+  LOADED = "LOADED",
+  ERROR = "ERROR",
+  LOADING = "LOADING",
+  NEVER = "NEVER",
+}
+
+export enum LoadingSendMessageState {
   LOADED = "LOADED",
   ERROR = "ERROR",
   LOADING = "LOADING",
@@ -22,4 +31,5 @@ export interface ICurrentConversationState {
   currentConversation: IConversation | undefined;
   messages: IMessage[];
   loadingState: LoadingCurrentConversationState;
+  loadingSendMessageState: LoadingSendMessageState;
 }
