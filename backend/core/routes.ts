9afe81acd5596b82c43613e8d3b2b4da485e6b72
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-30 15:55:54 +0400

    Add files to routes

--- backend/core/routes.ts
+++ backend/core/routes.ts
@@ -4,6 +4,7 @@ import {
   AuthController,
   CompanyController,
   ConversationController,
+  FileController,
   MarkedMessageController,
   UserController,
 } from "../controllers";
@@ -11,6 +12,7 @@ import MessagesController from "../controllers/MessagesController";
 import { authencticateToken } from "../utils/middleware/checkAuth";
 import { loginValidators } from "../validations/login";
 import { registerValidators } from "../validations/register";
+import upload from "./multer";
 
 const createRoutes = (app: express.Express, io: socket.Server) => {
   const AuthCtrl = new AuthController();
@@ -19,12 +21,16 @@ const createRoutes = (app: express.Express, io: socket.Server) => {
   const UserCtrl = new UserController();
   const MessagesCtrl = new MessagesController(io);
   const MarkedMessageCtrl = new MarkedMessageController(io);
+  const FileCtrl = new FileController();
 
   // # Users
   app.get("/api/users", UserCtrl.index);
   app.get("/api/users/:id", authencticateToken, UserCtrl.show);
-  app.get("/api/users/name/:query", authencticateToken, UserCtrl.showByNameOrEmail);
-
+  app.get(
+    "/api/users/name/:query",
+    authencticateToken,
+    UserCtrl.showByNameOrEmail
+  );
 
   //# Auth
   app.get("/api/auth/me", authencticateToken, AuthCtrl.getMe);
@@ -42,21 +48,40 @@ const createRoutes = (app: express.Express, io: socket.Server) => {
   //# Conversation
   app.get("/api/conversations", authencticateToken, ConversationCtrl.index);
   app.get("/api/conversations/:id", authencticateToken, ConversationCtrl.show);
-  app.get("/api/conversations/populated/:id", authencticateToken, ConversationCtrl.showPopulate);
-
+  app.get(
+    "/api/conversations/populated/:id",
+    authencticateToken,
+    ConversationCtrl.showPopulate
+  );
   app.post("/api/conversations", authencticateToken, ConversationCtrl.create);
-  app.patch("/api/conversations/joinall", authencticateToken, ConversationCtrl.joinAll);
-  app.patch("/api/conversations/addusers", authencticateToken, ConversationCtrl.addUsers);
+  app.patch(
+    "/api/conversations/joinall",
+    authencticateToken,
+    ConversationCtrl.joinAll
+  );
+  app.patch(
+    "/api/conversations/addusers",
+    authencticateToken,
+    ConversationCtrl.addUsers
+  );
 
-
-  //# Messages 
+  //# Messages
   app.get("/api/messages/:id", authencticateToken, MessagesCtrl.index);
+  app.get("/api/messages/unread/:id", authencticateToken, MessagesCtrl.getUnreadCountById);
   app.post("/api/messages/:id", authencticateToken, MessagesCtrl.create);
 
-  //# Mark Messages 
+  //# Mark Messages
   app.get("/api/mark", authencticateToken, MarkedMessageCtrl.index);
   app.post("/api/mark", authencticateToken, MarkedMessageCtrl.create);
   app.delete("/api/mark/:id", authencticateToken, MarkedMessageCtrl.delete);
+
+  //# Files
+  app.post(
+    "/api/files/upload",
+    authencticateToken,
+    upload.array("images", 10),
+    FileCtrl.upload
+  );
 };
 
 export default createRoutes;
