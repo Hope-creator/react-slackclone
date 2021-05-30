6a8b32c7119e49cc08905ba59bd670eaefc8b9d9
Hope-creator <goryachkin.ivan1@mail.ru>
2021-05-30 16:14:48 +0400

    Add connection error handler to socket/client

--- src/services/socket/socket.tsx
+++ src/services/socket/socket.tsx
@@ -1,5 +1,11 @@
 import io from "socket.io-client";
 
-const socket = io();
+const socket = io({reconnectionAttempts: 3});
+
+socket.on("connect_error", (err: any) => {
+  socket.disconnect()
+  console.log(err instanceof Error); // true
+  console.log(err.message); // not authorized
+});
 
 export default socket;
