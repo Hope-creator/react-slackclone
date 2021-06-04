82bd0a0a917a9f7a1300539295c7de720301a8f5
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-04 14:00:05 +0400

    Add read and unread to messages controller

--- backend/controllers/MessagesController.ts
+++ backend/controllers/MessagesController.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { isValidObjectId } from "mongoose";
 import socket from "socket.io";
+import { mongoose } from "../core/db";
 import { ConversationModel } from "../models/ConversationModel";
-import { MarkedMessageModel } from "../models/MarkedMessageModel";
 import { MessageModel } from "../models/MessageModel";
 
 class MessagesController {
@@ -35,27 +35,41 @@ class MessagesController {
               conversation.members.includes(userId)) ||
             !conversation.is_private
           ) {
-            const query = await MessageModel.find({
-              dest: conversation._id,
-            })
-              .populate("creator")
-              .populate("attachments")
-              .lean();
-            const messages = await Promise.all(
-              query.map(async (message) => {
-                const isMarked = await MarkedMessageModel.exists({
-                  user: userId,
-                  markedMessage: message._id,
-                });
-                if (isMarked) {
-                  message.marked = true;
-                } else {
-                  message.marked = false;
-                }
-                return message;
-              })
-            );
-            res.json({ status: "success", data: messages });
+            const query = await MessageModel.aggregate([
+              { $match: { dest: conversation._id } },
+              {
+                $addFields: {
+                  marked: {
+                    $in: [mongoose.Types.ObjectId(userId), "$markedBy"],
+                  },
+                },
+              },
+
+              {
+                $lookup: {
+                  from: "users",
+                  localField: "creator",
+                  foreignField: "_id",
+                  as: "creator",
+                },
+              },
+              { $unwind: "$creator" },
+              {
+                $lookup: {
+                  from: "files",
+                  localField: "attachments",
+                  foreignField: "_id",
+                  as: "attachments",
+                },
+              },
+              {
+                $project: {
+                  markedBy: 0,
+                  creator: { password: 0, status: 0 },
+                },
+              },
+            ]);
+            res.json({ status: "success", data: query });
           } else {
             res
               .status(404)
@@ -89,7 +103,7 @@ class MessagesController {
             .status(400)
             .json({ status: "error", data: "Conversations doesn't exist" });
         } else {
-          const count = await MessageModel.count({
+          const count = await MessageModel.countDocuments({
             dest: conversationsId,
             unreadBy: req.userId,
           });
@@ -102,6 +116,122 @@ class MessagesController {
         });
         console.log("Error on MessagesController / index:", error);
       }
+    }
+  };
+
+  getUnread = async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<void> => {
+    const userId = req.userId;
+    try {
+      const messages = await MessageModel.aggregate([
+        { $match: { unreadBy: mongoose.Types.ObjectId(userId) } },
+        {
+          $addFields: {
+            marked: {
+              $in: [mongoose.Types.ObjectId(userId), "$markedBy"],
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "creator",
+            foreignField: "_id",
+            as: "creator",
+          },
+        },
+        { $unwind: "$creator" },
+        {
+          $lookup: {
+            from: "files",
+            localField: "attachments",
+            foreignField: "_id",
+            as: "attachments",
+          },
+        },
+        {
+          $project: {
+            markedBy: 0,
+            creator: { password: 0, status: 0 },
+          },
+        },
+      ]);
+      res.json({ status: "success", data: messages });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        errors: JSON.stringify(error),
+      });
+      console.log("Error on MessagesController / index:", error);
+    }
+  };
+
+  readAll = async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<void> => {
+    const userId = req.userId;
+    try {
+      await MessageModel.updateMany(
+        { unreadBy: userId },
+        { $pull: { unreadBy: userId } }
+      );
+      res.json({ status: "success", data: true });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        data: false,
+      });
+      console.log("Error on MessagesController / index:", error);
+    }
+  };
+
+  readOneByMessageId = async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<void> => {
+    const userId = req.userId;
+    const messageId = req.body.messageId;
+    if (!isValidObjectId(messageId)) {
+      res.status(400).json({ status: "error", data: "Wrong type of ID" });
+    } else {
+      try {
+        const message = await MessageModel.findByIdAndUpdate(
+          { _id: messageId },
+          { $pull: { unreadBy: userId } },
+          { new: true }
+        )
+          .populate("creator")
+          .exec();
+          message && this.io.to(userId).emit("SERVER:READ_ONE", message.dest)
+        res.json({ status: "success", data: true });
+      } catch (error) {
+        res.status(500).json({
+          status: "error",
+          data: false,
+        });
+        console.log("Error on MessagesController / index:", error);
+      }
+    }
+  };
+
+  readAllByConversationId = async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<void> => {
+    const userId = req.userId;
+    const convId = req.body.conversationId;
+    try {
+      await MessageModel.updateMany({ unreadBy: userId, dest: convId });
+      res.json({ status: "success", data: true });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        data: false,
+      });
+      console.log("Error on MessagesController / index:", error);
     }
   };
 
@@ -143,15 +273,55 @@ class MessagesController {
               undefined,
             attachments: req.body.attachments,
           };
-          const message = await (await new MessageModel(postData).save())
-            .populate("creator")
-            .populate("attachments")
-            .execPopulate();
+          const data = await new MessageModel(postData).save();
+          const aggregateArr = await MessageModel.aggregate([
+            { $match: { _id: data._id } },
+            {
+              $addFields: {
+                marked: {
+                  $in: [mongoose.Types.ObjectId(userId), "$markedBy"],
+                },
+              },
+            },
+
+            {
+              $lookup: {
+                from: "users",
+                localField: "creator",
+                foreignField: "_id",
+                as: "creator",
+              },
+            },
+            { $unwind: "$creator" },
+            {
+              $lookup: {
+                from: "files",
+                localField: "attachments",
+                foreignField: "_id",
+                as: "attachments",
+              },
+            },
+            {
+              $project: {
+                markedBy: 0,
+                creator: { password: 0, status: 0 },
+              },
+            },
+          ]).exec();
+          const message = aggregateArr[0];
           this.io
             .to(message.dest.toString())
             .emit("SERVER:NEW_MESSAGE", message);
           if (!conversation.is_channel)
-            this.io.emit("SERVER:NEW_UNREAD", conversation._id);
+            this.io
+              .to(
+                conversation.members
+                  .filter(
+                    (_userId) => _userId.toString() !== userId.toString()
+                  )[0]
+                  .toString()
+              )
+              .emit("SERVER:NEW_UNREAD", message);
           res.json({ status: "success", data: message });
         }
       } catch (error) {
