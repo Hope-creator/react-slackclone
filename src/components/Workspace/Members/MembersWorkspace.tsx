1af10fac0e6b41bc710ba8c03e5d2e23b1d82477
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-04 15:21:32 +0400

    Refactor members workspace

--- src/components/Workspace/Members/MembersWorkspace.tsx
+++ src/components/Workspace/Members/MembersWorkspace.tsx
@@ -1,37 +1,63 @@
-import React from 'react'
-import { IUser } from '../../../store/modules/user/types'
+import React from "react";
 
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import { WorkspaceContent } from '../WorkspaceContent'
-import { WorkspaceHeader } from '../WorkspaceHeader'
-import { MembersContent } from './Content/MembersContent'
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { WorkspaceContent } from "../WorkspaceContent";
+import { WorkspaceHeader } from "../WorkspaceHeader";
+import { MembersContent } from "./Content/MembersContent";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  clearCurrentMembers,
+  fetchCurrentMembers,
+} from "../../../store/modules/currentMembers/currentMembers";
+import {
+  selectCurrentMembers,
+  selectCurrentMembersLoadingState,
+} from "../../../store/modules/currentMembers/selectors";
+import { LoadingCurrentMembersState } from "../../../store/modules/currentMembers/types";
+import { CircularProgress } from "@material-ui/core";
 
-interface IMembersWorkspaceProps {
-    currentMembers: IUser[]
-}
+export const MembersWorkspace = () => {
+  const dispatch = useDispatch();
 
-export const MembersWorkspace: React.FC<IMembersWorkspaceProps> = ({currentMembers}) => {
-    return (
-        <>
-        <WorkspaceHeader
-          leftSideContent={
-            <>
-              <Typography variant="h6">People</Typography>
-              <Typography variant="caption">
-                {currentMembers.length} members
-              </Typography>
-            </>
-          }
-          rightSideContent={
-            <>
-              <Button>Invite people</Button>
-            </>
-          }
-        />
-        <WorkspaceContent
-          children={<MembersContent members={currentMembers} />}
-        />
-      </>
-    )
-}
+  const membersLoadingState = useSelector(selectCurrentMembersLoadingState);
+
+  const members = useSelector(selectCurrentMembers);
+
+  React.useEffect(() => {
+    dispatch(fetchCurrentMembers());
+
+    return function clearMembers() {
+      dispatch(clearCurrentMembers());
+    };
+  }, [dispatch]);
+
+  return (
+    <>
+      <WorkspaceHeader
+        leftSideContent={
+          <>
+            <Typography variant="h6">People</Typography>
+            <Typography variant="caption">
+              {members && members.length} members
+            </Typography>
+          </>
+        }
+        rightSideContent={
+          <>
+            <Button>Invite people</Button>
+          </>
+        }
+      />
+      <WorkspaceContent
+        children={
+          membersLoadingState === LoadingCurrentMembersState.LOADING ? (
+            <CircularProgress />
+          ) : (
+            <MembersContent members={members} />
+          )
+        }
+      />
+    </>
+  );
+};
