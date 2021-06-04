976e335b2d112ce2fbaf376a40e9693ff0ecd4dc
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-04 14:03:23 +0400

    Refactor main workspace

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,112 +1,28 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
-import socket from "../../services/socket/socket";
-import {
-  addNewMessage,
-  clearCurrentConversation,
-  fetchCurrentConversation,
-} from "../../store/modules/currentConversation/currentConversation";
-import {
-  selectCurrentConversation,
-  selectCurrentConversationLoadingState,
-  selectMessages,
-} from "../../store/modules/currentConversation/selectors";
-import {
-  IMessage,
-  LoadingCurrentConversationState,
-} from "../../store/modules/currentConversation/types";
-import {
-  clearCurrentMembers,
-  fetchCurrentMembers,
-} from "../../store/modules/currentMembers/currentMembers";
-import {
-  selectCurrentMembers,
-  selectCurrentMembersLoadingState,
-} from "../../store/modules/currentMembers/selectors";
-import { LoadingCurrentMembersState } from "../../store/modules/currentMembers/types";
+import { selectUser } from "../../store/modules/user/selectors";
 import { ConversationWorkspace } from "./Conversation/ConversationWorkspace";
-import { DirectMessageWorkspace } from "./Conversation/DirectMessageWorkspace";
+import { DefaultWorkspace } from "./DefaultWorkspace";
 import { MembersWorkspace } from "./Members/MembersWorkspace";
-import { WorkspaceContent } from "./WorkspaceContent";
-import { WorkspaceHeader } from "./WorkspaceHeader";
+import { UnreadsWorkspace } from "./Unreads/UnreadsWorkspace";
 
 export const Workspace = () => {
   const history = useHistory();
-  const dispatch = useDispatch();
   const path = history.location.pathname;
-  const messages = useSelector(selectMessages);
-  const currentConversation = useSelector(selectCurrentConversation);
-  const currentMembers = useSelector(selectCurrentMembers);
-  const currentMembersLoadingState = useSelector(
-    selectCurrentMembersLoadingState
-  );
-  const currentConversationLoadingState = useSelector(
-    selectCurrentConversationLoadingState
-  );
+  const user = useSelector(selectUser);
 
-  React.useEffect(() => {
-    if (currentConversation) {
-      socket.emit("CONVERSATION:JOIN", currentConversation._id);
-      socket.on("SERVER:NEW_MESSAGE", (message: IMessage) => {
-        console.log(message);
-        dispatch(addNewMessage(message));
-      });
-    }
-    return function clearConnect() {
-      socket.emit("CONVERSATION:LEAVE");
-      socket.removeListener("SERVER:NEW_MESSAGE");
-    };
-  }, [currentConversation, dispatch]);
-
-  React.useEffect(() => {
-    if (path.length === 25) {
-      const fetchPath = path.split("").slice(1).join("");
-      dispatch(fetchCurrentConversation(fetchPath));
-    }
-    return function clearConversation() {
-      dispatch(clearCurrentConversation());
-    };
-  }, [path, dispatch]);
-
-  React.useEffect(() => {
-    if (path.match("members")) {
-      dispatch(fetchCurrentMembers());
-    }
-    return function clearMembers() {
-      dispatch(clearCurrentMembers());
-    };
-  }, [path, dispatch]);
-
-  if (
-    currentConversation &&
-    messages &&
-    currentConversationLoadingState === LoadingCurrentConversationState.LOADED
-  ) {
-    return currentConversation.is_channel ? (
-      <ConversationWorkspace
-        conversation={currentConversation}
-        messages={messages}
-      />
-    ) : (
-      <DirectMessageWorkspace
-        conversation={currentConversation}
-        messages={messages}
-      />
-    );
+  if (user && path.length === 25) {
+    return <ConversationWorkspace user={user} />;
   }
 
-  if (
-    currentMembers &&
-    currentMembersLoadingState === LoadingCurrentMembersState.LOADED
-  ) {
-    return <MembersWorkspace currentMembers={currentMembers} />;
+  if (user && path.match("members")) {
+    return <MembersWorkspace />;
   }
 
-  return (
-    <>
-      <WorkspaceHeader />
-      <WorkspaceContent />
-    </>
-  );
+  if (user && path.match("unreads")) {
+    return <UnreadsWorkspace user={user} />;
+  }
+
+  return <DefaultWorkspace />;
 };
