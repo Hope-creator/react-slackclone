1bfa122d33b5db0907960486e68413b0c8a7c3e3
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-11 14:42:38 +0400

    Add dialog workspace and marked messages workspace to main workspace handle

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -4,6 +4,8 @@ import { useHistory } from "react-router";
 import { selectUser } from "../../store/modules/user/selectors";
 import { ConversationWorkspace } from "./Conversation/ConversationWorkspace";
 import { DefaultWorkspace } from "./DefaultWorkspace";
+import { DialogWorkspace } from "./Dialog/DialogWorkspace";
+import { MarkedWorkspace } from "./Marked/MarkedWorkspace";
 import { MembersWorkspace } from "./Members/MembersWorkspace";
 import { UnreadsWorkspace } from "./Unreads/UnreadsWorkspace";
 
@@ -11,9 +13,13 @@ export const Workspace = () => {
   const history = useHistory();
   const path = history.location.pathname;
   const user = useSelector(selectUser);
-
-  if (user && path.length === 25) {
+  
+  if (user && path.match(/^\/\w{24}$/)) {
     return <ConversationWorkspace user={user} />;
+  }
+
+  if (user && path.match(/^\/d\/\w{24}$/)) {
+    return <DialogWorkspace user={user} />;
   }
 
   if (user && path.match("members")) {
@@ -22,6 +28,10 @@ export const Workspace = () => {
 
   if (user && path.match("unreads")) {
     return <UnreadsWorkspace user={user} />;
+  }
+
+  if (user && path.match("saved-page")) {
+    return <MarkedWorkspace user={user} />;
   }
 
   return <DefaultWorkspace />;
