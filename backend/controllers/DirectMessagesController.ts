3f5e801047664b38442be9c2f02b930860e39592
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-11 14:16:00 +0400

    Create DM controller

---
+++ backend/controllers/DirectMessagesController.ts
@@ -0,0 +1,109 @@
+import express from "express";
+import { isValidObjectId } from "mongoose";
+import socket from "socket.io";
+import { DialogModel } from "../models/DialogModel";
+import { MessageModel } from "../models/MessageModel";
+import { UserModel } from "../models/UserModel";
+import { getAggregateMessage } from "../utils/function/getAggregateMessage";
+
+class DirectMessagesController {
+  io: socket.Server;
+
+  constructor(io: socket.Server) {
+    this.io = io;
+  }
+
+  index = async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<void> => {
+    const user = req.user;
+    const dialogId = req.params.id;
+    if (!isValidObjectId(dialogId)) {
+      res.status(400).json({ status: "error", data: "Wrong type of ID" });
+    } else {
+      try {
+        const dialog = await DialogModel.findOne({
+          _id: dialogId,
+          members: user._id,
+        }).exec();
+        if (!dialog) {
+          res
+            .status(400)
+            .json({ status: "error", data: "Dialog doesn't exist" });
+          return;
+        }
+        const messages = await getAggregateMessage(
+          { dest: dialog._id },
+          user._id
+        );
+        res.json({ status: "success", data: messages });
+      } catch (error) {
+        res.status(500).json({
+          status: "error",
+          errors: JSON.stringify(error),
+        });
+        console.log("Error on MessagesController / index:", error);
+      }
+    }
+  };
+
+  create = async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<void> => {
+    const user = req.user;
+    const dialogsId = req.body.id;
+    if (!isValidObjectId(dialogsId)) {
+      res.status(400).json({ status: "error", data: "Wrong type of ID" });
+      return;
+    }
+    try {
+      const dialog = await DialogModel.findOne({
+        $and: [{ _id: dialogsId }, { members: user._id }],
+      }).exec();
+      if (!dialog) {
+        res.status(400).json({ status: "error", data: "Dialog doesn't exist" });
+      } else {
+        let unreadBy = undefined;
+        const partnerId = dialog.members.filter(
+          (id) => id.toString() !== user._id.toString()
+        )[0];
+        if (partnerId) {
+          unreadBy = await UserModel.findOne({
+            _id: partnerId,
+          });
+        }
+        const postData = {
+          creator: req.userId,
+          dest: dialog.id,
+          text: req.body.text,
+          unreadBy: unreadBy,
+          attachments: req.body.attachments,
+        };
+        const data = await new MessageModel(postData).save();
+        const aggregateArr = await getAggregateMessage(
+          { _id: data._id },
+          user._id
+        );
+        const message = aggregateArr[0];
+        this.io.to(message.dest.toString()).emit("SERVER:NEW_MESSAGE", message);
+        // Check on user
+        if (unreadBy) {
+          this.io
+            .to(unreadBy._id.toString())
+            .emit("SERVER:NEW_UNREAD", message);
+        }
+        res.json({ status: "success", data: message });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        errors: JSON.stringify(error),
+      });
+      console.log("Error on MessagesController / create:", error);
+    }
+  };
+}
+
+export default DirectMessagesController;
