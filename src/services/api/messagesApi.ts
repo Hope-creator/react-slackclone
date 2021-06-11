85eef533d35adc9815252cdaf2200aff2e5e5bc6
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-11 15:03:02 +0400

    Add fetch and send dm in messages api

--- src/services/api/messagesApi.ts
+++ src/services/api/messagesApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { ISendMessageForm } from "../../components/SendMessageForm";
-import { IMessage } from "../../store/modules/currentConversation/types";
+import { IMessage } from "../../store/modules/messages/types";
 import { IResponse } from "./types";
 
 export const messagesApi = {
@@ -10,13 +10,19 @@ export const messagesApi = {
     );
     return response.data.data;
   },
+  async fetchDirectMessages(id: string): Promise<IMessage[] | []> {
+    const response = await axios.get<IResponse<IMessage[] | []>>(
+      `/api/messages/dm/${id}`
+    );
+    return response.data.data;
+  },
   async sendMessage({
-    conversationId,
+    dest,
     text,
     attachments,
   }: ISendMessageForm): Promise<IMessage> {
     const response = await axios.post<IResponse<IMessage>>(
-      `/api/messages/${conversationId}`,
+      `/api/messages/${dest}`,
       {
         text,
         attachments,
@@ -24,21 +30,72 @@ export const messagesApi = {
     );
     return response.data.data;
   },
-  async getUnread(conversationId: string): Promise<number> {
+  async sendDirectMessage({
+    dest,
+    text,
+    attachments,
+  }: ISendMessageForm): Promise<IMessage> {
+    const response = await axios.post<IResponse<IMessage>>(
+      `/api/messages/dm/`,
+      {
+        id: dest,
+        text,
+        attachments,
+      }
+    );
+    return response.data.data;
+  },
+  async getUnreadCount(conversationId: string): Promise<number> {
     const response = await axios.get<IResponse<number>>(
-      `/api/messages/unread/${conversationId}`
+      `/api/messages/unread/count/${conversationId}`
+    );
+    return response.data.data;
+  },
+  async getAllUnread(): Promise<IMessage[] | []> {
+    const response = await axios.get<IResponse<IMessage[] | []>>(
+      `/api/messages/unread`
+    );
+    return response.data.data;
+  },
+  async readAll(): Promise<boolean> {
+    const response = await axios.patch<IResponse<boolean>>(
+      `/api/messages/unread`
+    );
+    return response.data.data;
+  },
+  async readAllByConversationId(conversationId: string): Promise<boolean> {
+    const response = await axios.patch<IResponse<boolean>>(
+      `/api/messages/unread?conversation=${conversationId}`,
+      conversationId
+    );
+    return response.data.data;
+  },
+  async readOneByMessageId(messageId: string): Promise<boolean> {
+    const response = await axios.patch<IResponse<boolean>>(
+      `/api/messages/unread/one`,
+      { messageId: messageId }
+    );
+    return response.data.data;
+  },
+  async getMarkMessages(): Promise<IMessage[]> {
+    const response = await axios.get<IResponse<IMessage[]>>(
+      `/api/messages/mark`
     );
     return response.data.data;
   },
   async markMessage(messageId: string): Promise<boolean> {
-    const response = await axios.post<IResponse<boolean>>(`/api/mark`, {
-      id: messageId,
-    });
+    const response = await axios.post<IResponse<boolean>>(
+      `/api/messages/mark`,
+      {
+        messageId: messageId,
+      }
+    );
     return response.data.data;
   },
   async unmarkMessage(messageId: string): Promise<boolean> {
-    const response = await axios.delete<IResponse<boolean>>(
-      `/api/mark/${messageId}`
+    const response = await axios.patch<IResponse<boolean>>(
+      `/api/messages/mark`,
+      { messageId: messageId }
     );
     return response.data.data;
   },
