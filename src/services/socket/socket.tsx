ec65de0d0f45d4a46a30881946b6848cce2efd6a
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-18 13:10:05 +0400

    Add connection flag to socket frontend

--- src/services/socket/socket.tsx
+++ src/services/socket/socket.tsx
@@ -2,8 +2,13 @@ import io from "socket.io-client";
 
 const socket = io({reconnectionAttempts: 3});
 
+socket.on("connect",()=> {
+  sessionStorage.setItem("connection", "1");
+})
+
 socket.on("connect_error", (err: any) => {
-  socket.disconnect()
+  socket.disconnect();
+  sessionStorage.setItem("connection", "0");
   console.log(err instanceof Error); // true
   console.log(err.message); // not authorized
 });
