36af75e15b477a598e328785a4e4691b93da470d
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-18 13:04:11 +0400

    Add dialog get id to api, create dialog not in use and commented

--- src/services/api/dialogsApi.ts
+++ src/services/api/dialogsApi.ts
@@ -3,20 +3,26 @@ import { IDialog } from "../../store/modules/dialogs/types";
 import { IResponse } from "./types";
 
 export const dialogsApi = {
-  async fetchDialogs(): Promise<IDialog[] | []> {
-    const response = await axios.get<IResponse<IDialog[] | []>>(`/api/dialogs`);
-    return response.data.data;
-  },
-  async fetchDialog(id: string): Promise<IDialog[] | []> {
+  async fetchDialogs(search: string = ""): Promise<IDialog[] | []> {
     const response = await axios.get<IResponse<IDialog[] | []>>(
-      `/api/dialogs/${id}`
+      `/api/dialogs${search && "?search=" + search}`
     );
     return response.data.data;
   },
-  async createDialog(userId: string): Promise<IDialog> {
+  async fetchDialog(id: string): Promise<IDialog> {
+    const response = await axios.get<IResponse<IDialog>>(`/api/dialogs/${id}`);
+    return response.data.data;
+  },
+  async getDialogId(userId: string): Promise<string> {
+    const response = await axios.get<IResponse<string>>(
+      `/api/dialogs/getid/${userId}`
+    );
+    return response.data.data;
+  },
+  /*async createDialog(userId: string): Promise<IDialog> {
     const response = await axios.post<IResponse<IDialog>>(`/api/dialogs/`, {
       id: userId,
     });
     return response.data.data;
-  },
+  },*/
 };
