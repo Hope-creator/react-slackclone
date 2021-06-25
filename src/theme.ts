0393c9488be46ed211999136a4294920d342bf73
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-25 10:50:16 +0400

    Prettier

--- src/theme.ts
+++ src/theme.ts
@@ -4,26 +4,24 @@ import { createMuiTheme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: [
-      'Lato'
-    ].join(","),
+    fontFamily: ["Lato"].join(","),
     h3: {
-      fontWeight: 700
+      fontWeight: 700,
     },
     h5: {
-      fontWeight: 700
+      fontWeight: 700,
     },
     h6: {
       fontSize: "1rem",
       fontWeight: 700,
-      color: "#5B5A5B"
-    }
+      color: "#5B5A5B",
+    },
   },
   palette: {
     primary: {
       light: "#A493A7",
       main: /*"#611f69"*/ "rgb(136,101,135)",
-      dark: "#4A154B"
+      dark: "#4A154B",
     },
     secondary: {
       main: "#4285f4",
@@ -51,66 +49,70 @@ export const theme = createMuiTheme({
           boxShadow: "0px 0px 0px 4px rgba(29,155,209, 0.3)",
         },
         "&:hover": {
-          color: "#4285f4"
-        }
+          color: "#4285f4",
+        },
       },
     },
     MuiSvgIcon: {
-      fontSizeSmall:
-      {
-        fontSize: 11
-      }
+      fontSizeSmall: {
+        fontSize: 11,
+      },
     },
     MuiIconButton: {
       root: {
         borderRadius: 0,
-        fontSize: "0.8rem"
+        fontSize: "0.8rem",
       },
       label: {
         flexDirection: "column",
-        maxWidth: "fit-content"
-      }
+        maxWidth: "fit-content",
+      },
     },
-    MuiMenuItem:{
+    MuiMenuItem: {
       root: {
         "&:hover": {
           backgroundColor: "#4285f4",
-          color: "#fff"
-        }
-      }
+          color: "#fff",
+        },
+      },
     },
     MuiListItem: {
       button: {
         "&:hover": {
-          backgroundColor: "rgba(29,155,209, 0.3)"
-        }
-      }
+          backgroundColor: "rgba(29,155,209, 0.3)",
+        },
+      },
     },
     MuiListItemIcon: {
       root: {
-        minWidth: 35
-      }
+        minWidth: 35,
+      },
     },
     MuiAccordion: {
-      root:{
-        '&$expanded': {
-          margin: 'auto',
-        }
-      }
+      root: {
+        "&$expanded": {
+          margin: "auto",
+        },
+      },
     },
     MuiTypography: {
       body1: {
         fontSize: "0.9rem",
-        wordBreak: "break-word"
+        wordBreak: "break-word",
       },
       subtitle2: {
-        fontWeight: 700
-      }
+        fontWeight: 700,
+      },
     },
     MuiPaper: {
       elevation4: {
-        boxShadow: 0
-      }
-    }
+        boxShadow: 0,
+      },
+    },
+    MuiListItemAvatar: {
+      root: {
+        minWidth: 33,
+      },
+    },
   },
 });
