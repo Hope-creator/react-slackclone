1578ba4f3d29b106f14deacb78ab77d27f536f9b
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-25 14:07:06 +0400

    Add pagination in user api

--- src/services/api/userApi.ts
+++ src/services/api/userApi.ts
@@ -1,18 +1,28 @@
-import { IUser } from './../../store/modules/user/types';
-import axios from "axios"; 
-import { IResponse } from './types';
+import { IUser } from "./../../store/modules/user/types";
+import axios from "axios";
+import { IPaginationData, IResponse, IResponsePagination } from "./types";
 
 export const userApi = {
-    async getUser (id: string): Promise<IUser> {
-        const response = await axios.get<IResponse<IUser>>(`/api/users/${id}`);
-        return response.data.data
-    },
-    async getAllUsers (): Promise<IUser[]> {
-        const response = await axios.get<IResponse<IUser[]>>(`/api/users`);
-        return response.data.data
-    },
-    async getUsersByNameOrEmail (nameOrEmail: string): Promise<IUser[]> {
-        const response = await axios.get<IResponse<IUser[]>>(`/api/users/name/${nameOrEmail}`);
-        return response.data.data
-    }
-}
+  async getUser(id: string): Promise<IUser> {
+    const response = await axios.get<IResponse<IUser>>(`/api/users/${id}`);
+    return response.data.data;
+  },
+  async getAllUsers(
+    search: string = "",
+    page: number = 1,
+    count: number = 20
+  ): Promise<IPaginationData<IUser[]>> {
+    const response = await axios.get<IResponsePagination<IUser[]>>(
+      `/api/users${"?page=" + page + "&count=" + count}${
+        search && "&search=" + search
+      }`
+    );
+    return response.data.data;
+  },
+  async getUsersByNameOrEmail(nameOrEmail: string): Promise<IUser[]> {
+    const response = await axios.get<IResponse<IUser[]>>(
+      `/api/users/name/${nameOrEmail}`
+    );
+    return response.data.data;
+  },
+};
