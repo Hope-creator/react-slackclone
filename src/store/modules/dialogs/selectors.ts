656b6439b1e0de852b80b95fcc4fe3c1752de39e
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-25 14:13:38 +0400

    Delete dialogs module and api

--- src/store/modules/dialogs/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { IRootState } from "../../store";
-
-export const selectDialogsState = (state: IRootState) => state.dialogs;
-
-export const selectDialogs = (state: IRootState) =>
-  selectDialogsState(state).dialogs;
-
-export const selectDialogsLoadingState = (state: IRootState) =>
-  selectDialogsState(state).loadingState;
