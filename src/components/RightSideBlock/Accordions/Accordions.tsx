05eba6e9ebe2fb080cb5eb04e4c901a24126bbb1
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-25 11:21:39 +0400

    Delete pinned

--- src/components/RightSideBlock/Accordions/Accordions.tsx
+++ src/components/RightSideBlock/Accordions/Accordions.tsx
@@ -3,7 +3,6 @@ import { IConversation } from "../../../store/modules/conversations/types";
 
 import { About } from "./About";
 import { Members } from "./Members";
-import { Pinned } from "./Pinned";
 
 interface IAccordionsProps {
   channel: IConversation;
@@ -16,7 +15,6 @@ export const Accordeons: React.FC<IAccordionsProps> = ({
     <>
       <About channel={channel} />
       <Members channel={channel} />
-      <Pinned />
     </>
   );
 };
