eda4fed5e0ec2d766290684ca009b48b1c7955d6
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-25 10:33:36 +0400

    Add pagination to direct messages ctrl

--- backend/controllers/DirectMessagesController.ts
+++ backend/controllers/DirectMessagesController.ts
@@ -5,6 +5,7 @@ import { DialogModel } from "../models/DialogModel";
 import { MessageModel } from "../models/MessageModel";
 import { UserModel } from "../models/UserModel";
 import { getAggregateMessage } from "../utils/function/getAggregateMessage";
+import { getAggregateMessageWithPagination } from "../utils/function/getAggregateMessageWithPagination";
 
 class DirectMessagesController {
   io: socket.Server;
@@ -18,24 +19,30 @@ class DirectMessagesController {
     res: express.Response
   ): Promise<void> => {
     const user = req.user;
-    const dialogId = req.params.id;
-    if (!isValidObjectId(dialogId)) {
+    const partnerId = req.params.id;
+    const { page = 1, count = 40 } = req.query;
+    const skipPage = page > 0 ? (Number(page) - 1) * Number(count) : 0;
+    if (!isValidObjectId(partnerId)) {
       res.status(400).json({ status: "error", data: "Wrong type of ID" });
     } else {
       try {
-        const dialog = await DialogModel.findOne({
-          _id: dialogId,
-          $or: [{ creator: user._id }, { partner: user._id }],
+        const partner = await UserModel.findOne({
+          _id: partnerId,
         }).exec();
-        if (!dialog) {
-          res
-            .status(400)
-            .json({ status: "error", data: "Dialog doesn't exist" });
+        if (!partner) {
+          res.status(400).json({ status: "error", data: "User doesn't exist" });
           return;
         }
-        const messages = await getAggregateMessage(
-          { dest: dialog._id },
-          user._id
+        const messages = await getAggregateMessageWithPagination(
+          {
+            $or: [
+              { creator: user._id, dest: partner._id },
+              { creator: partner._id, dest: user._id },
+            ],
+          },
+          user._id,
+          skipPage,
+          Number(count)
         );
         res.json({ status: "success", data: messages });
       } catch (error) {
@@ -53,21 +60,19 @@ class DirectMessagesController {
     res: express.Response
   ): Promise<void> => {
     const user = req.user;
-    const dialogsId = req.body.id;
-    if (!isValidObjectId(dialogsId)) {
+    const partnerId = req.body.id;
+    if (!isValidObjectId(partnerId)) {
       res.status(400).json({ status: "error", data: "Wrong type of ID" });
       return;
     }
     try {
-      const dialog = await DialogModel.findOne({
-        _id: dialogsId,
-        $or: [{ creator: user._id }, { partner: user._id }],
+      const partner = await UserModel.findOne({
+        _id: partnerId,
       }).exec();
-      if (!dialog) {
-        res.status(400).json({ status: "error", data: "Dialog doesn't exist" });
+      if (!partner) {
+        res.status(400).json({ status: "error", data: "User doesn't exist" });
       } else {
-        let unreadBy = undefined;
-        const partnerId =
+        /*const partnerId =
           dialog.creator.toString() !== user._id.toString()
             ? dialog.creator
             : dialog.partner;
@@ -75,10 +80,11 @@ class DirectMessagesController {
           unreadBy = await UserModel.findOne({
             _id: partnerId,
           });
-        }
+        }*/
+        const unreadBy = partner._id;
         const postData = {
           creator: req.userId,
-          dest: dialog.id,
+          dest: partner._id,
           text: req.body.text,
           unreadBy: unreadBy,
           attachments: req.body.attachments,
@@ -89,20 +95,16 @@ class DirectMessagesController {
           user._id
         );
         const message = aggregateArr[0];
-        await DialogModel.findByIdAndUpdate(
+        /*await DialogModel.findByIdAndUpdate(
           dialog._id,
           { last_message: data._id },
           { new: true }
-        );
+        );*/
         this.io
-          .to(message.dest.toString())
+          .to([message.dest.toString(), user._id.toString()])
           .emit("SERVER:NEW_DIRECTMESSAGE", message);
-        // Check on user
-        if (unreadBy) {
-          this.io
-            .to(unreadBy._id.toString())
-            .emit("SERVER:NEW_UNREAD", message);
-        }
+
+        this.io.to(unreadBy.toString()).emit("SERVER:NEW_UNREAD", message);
         res.json({ status: "success", data: message });
       }
     } catch (error) {
