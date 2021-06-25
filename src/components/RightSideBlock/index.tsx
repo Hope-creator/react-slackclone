80a8b5632ea94fc64736032eaf320b206a681bab
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-25 11:20:50 +0400

    Add conversation update socket listener to right side block

--- src/components/RightSideBlock/index.tsx
+++ src/components/RightSideBlock/index.tsx
@@ -1,6 +1,9 @@
 import { Box } from "@material-ui/core";
 import React from "react";
+import { useDispatch } from "react-redux";
+import socket from "../../services/socket/socket";
 import { IConversation } from "../../store/modules/conversations/types";
+import { updateCurrentInfoConversation } from "../../store/modules/currentInfo_side/currentInfo";
 import { InfoItemTypeState } from "../../store/modules/currentInfo_side/types";
 import { IUser } from "../../store/modules/user/types";
 import { Accordeons } from "./Accordions/Accordions";
@@ -11,20 +14,45 @@ import ProfileInfo from "./ProfileInfo";
 
 interface RightSideBlockProps {
   headerSubText?: string;
-  type: string;
+  type: InfoItemTypeState;
   item: IUser | IConversation;
 }
 
 export const RightSideBlock: React.FC<RightSideBlockProps> = ({
   type,
-  item
+  item,
 }) => {
+  const dispatch = useDispatch();
+
+  const conversationUpdateHandleListener = React.useCallback(
+    (_conversation: IConversation) => {
+      if (
+        type === InfoItemTypeState.CHANNEL &&
+        item._id === _conversation._id
+      ) {
+        dispatch(updateCurrentInfoConversation(_conversation));
+      }
+    },
+    [dispatch, type, item._id]
+  );
+
+  React.useEffect(() => {
+    if (type === InfoItemTypeState.CHANNEL) {
+      socket.on("SERVER:CONVERSATION_UPDATE", conversationUpdateHandleListener);
+    }
+    return function clearConnect() {
+      socket.removeListener(
+        "SERVER:CONVERSATION_UPDATE",
+        conversationUpdateHandleListener
+      );
+    };
+  }, [dispatch, conversationUpdateHandleListener, type]);
 
   if (type === InfoItemTypeState.CHANNEL) {
     return (
       <Box>
         <Header headerSubText={item.name} headerTitle="Details" />
-        <ContentButtons name={item.name} />
+        <ContentButtons channel={item as IConversation} />
         <Accordeons channel={item as IConversation} />
         <Files />
       </Box>
