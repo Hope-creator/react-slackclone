8387aa588d5bd125a626c35d3f71cd10c2c69e4f
Hope-creator <goryachkin.ivan1@mail.ru>
2021-06-25 14:06:07 +0400

    Add pagination response in api types

--- src/services/api/types.ts
+++ src/services/api/types.ts
@@ -1,9 +1,19 @@
 export enum ResponseStatus {
-    SUCCESS = "success",
-    ERROR = "error"
+  SUCCESS = "success",
+  ERROR = "error",
 }
 
 export interface IResponse<T> {
-    status: ResponseStatus,
-    data: T,
+  status: ResponseStatus;
+  data: T;
+}
+
+export interface IPaginationData<T> {
+  results: T;
+  totalCount?: number;
+}
+
+export interface IResponsePagination<T> {
+    status: ResponseStatus;
+    data: IPaginationData<T>;
 }
