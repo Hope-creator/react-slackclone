6127544198f861c3dceb6c617c753bdc0c2ae1d9
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:01:05 +0400

    Add member kicked handler in members modal

--- src/components/MembersModal.tsx
+++ src/components/MembersModal.tsx
@@ -20,8 +20,10 @@ import {
 import { LoadingConversationsMembersState } from "../store/modules/conversationMembers/types";
 import { IConversation } from "../store/modules/conversations/types";
 import {
+  addOneConversationMembers,
   clearConversationMembers,
-  fetchConverastionMembers,
+  deleteOneConversationMembers,
+  fetchConversationMembers,
 } from "../store/modules/conversationMembers/conversationMembers";
 import { IUser } from "../store/modules/user/types";
 import socket from "../services/socket/socket";
@@ -45,6 +47,12 @@ const useStyles = makeStyles((theme: Theme) =>
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
       transform: `translate(-50%, -50%)`,
+      [theme.breakpoints.down("sm")]: {
+        width: 350,
+      },
+      [theme.breakpoints.down("xs")]: {
+        width: 250,
+      },
     },
     headerText: {
       width: "600px",
@@ -76,7 +84,7 @@ export const MembersModal: React.FC<IMembersModalProps> = ({
   );
 
   React.useEffect(() => {
-    dispatch(fetchConverastionMembers(conversation._id));
+    dispatch(fetchConversationMembers(conversation._id));
     return function cleanup() {
       dispatch(clearConversationMembers());
     };
@@ -85,7 +93,7 @@ export const MembersModal: React.FC<IMembersModalProps> = ({
   const handleListenerNewUsers = React.useCallback(
     (conversationId: string) => {
       if (conversationId === conversation._id)
-        dispatch(fetchConverastionMembers(conversation._id));
+        dispatch(fetchConversationMembers(conversation._id));
     },
     [dispatch, conversation._id]
   );
@@ -93,19 +101,30 @@ export const MembersModal: React.FC<IMembersModalProps> = ({
   const handleListenerNewUser = React.useCallback(
     (user: IUser, conversationId: string) => {
       if (conversationId === conversation._id)
-        dispatch(fetchConverastionMembers(conversation._id));
+        dispatch(addOneConversationMembers(user));
+    },
+    [dispatch, conversation._id]
+  );
+
+  const handleListenerKickUser = React.useCallback(
+    (user: IUser, conversationId: string) => {
+      if (conversationId === conversation._id)
+        dispatch(deleteOneConversationMembers(user));
     },
     [dispatch, conversation._id]
   );
 
   React.useEffect(() => {
-    if (conversation) {
-      socket.emit("CONVERSATION:JOIN", conversation._id);
-      socket.on("SERVER:NEW_CONVERSATION_MEMBER", handleListenerNewUser);
-      socket.on("SERVER:NEW_CONVERSATION_MEMBER_MANY", handleListenerNewUsers);
-    }
+    socket.addEventListener(
+      "SERVER:NEW_CONVERSATION_MEMBER",
+      handleListenerNewUser
+    );
+    socket.addEventListener(
+      "SERVER:NEW_CONVERSATION_MEMBER_MANY",
+      handleListenerNewUsers
+    );
+    socket.addEventListener("SERVER:MEMBER_KICKED", handleListenerKickUser);
     return () => {
-      socket.emit("CONVERSATION:LEAVE");
       socket.removeListener(
         "SERVER:NEW_CONVERSATION_MEMBER",
         handleListenerNewUser
@@ -114,8 +133,14 @@ export const MembersModal: React.FC<IMembersModalProps> = ({
         "SERVER:NEW_CONVERSATION_MEMBER_MANY",
         handleListenerNewUsers
       );
+      socket.removeListener("SERVER:MEMBER_KICKED", handleListenerKickUser);
     };
-  }, [dispatch, conversation, handleListenerNewUsers, handleListenerNewUser]);
+  }, [
+    dispatch,
+    handleListenerNewUsers,
+    handleListenerNewUser,
+    handleListenerKickUser,
+  ]);
 
   const handleOpen = () => {
     setOpen(true);
@@ -130,53 +155,9 @@ export const MembersModal: React.FC<IMembersModalProps> = ({
   };
 
   const itemKey = (index: number, data: IUser[]) => {
-      const user = data[index];
-      return user._id;
+    const user = data[index];
+    return user._id;
   };
-
-  const children = () => (
-    <Paper className={classes.paper}>
-      {membersLoadingState === LoadingConversationsMembersState.LOADING ? (
-        <CircularProgress />
-      ) : (
-        <>
-          <Grid
-            alignItems="center"
-            container
-            justify="space-between"
-            wrap="nowrap"
-          >
-            <Typography className={classes.headerText} variant="h6">
-              {members.length} members in #{conversation.name}
-            </Typography>
-            <IconButton onClick={handleClose}>
-              <CloseIcon />
-            </IconButton>
-          </Grid>
-          <UsersSearchForm formSubmit={changeFilterName} />
-          {me && (
-            <List
-              height={240}
-              itemCount={members.length}
-              itemSize={60}
-              itemData={members}
-              itemKey={itemKey}
-              width="inherit"
-            >
-              {({ index, style }) => {
-                const user = members[index];
-                return (
-                  <div style={style}>
-                    <UserListItem user={user} isMe={me._id === user._id} />
-                  </div>
-                );
-              }}
-            </List>
-          )}
-        </>
-      )}
-    </Paper>
-  );
 
   return (
     <div>
@@ -187,7 +168,51 @@ export const MembersModal: React.FC<IMembersModalProps> = ({
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        {children()}
+        <Paper className={classes.paper}>
+          {membersLoadingState === LoadingConversationsMembersState.LOADING ? (
+            <CircularProgress />
+          ) : (
+            <>
+              <Grid
+                alignItems="center"
+                container
+                justify="space-between"
+                wrap="nowrap"
+              >
+                <Typography className={classes.headerText} variant="h6">
+                  {members.length} members in #{conversation.name}
+                </Typography>
+                <IconButton onClick={handleClose}>
+                  <CloseIcon />
+                </IconButton>
+              </Grid>
+              <UsersSearchForm formSubmit={changeFilterName} />
+              {me && (
+                <List
+                  height={240}
+                  itemCount={members.length}
+                  itemSize={60}
+                  itemData={members}
+                  itemKey={itemKey}
+                  width="inherit"
+                >
+                  {({ index, style }) => {
+                    const user = members[index];
+                    return (
+                      <div style={style}>
+                        <UserListItem
+                          conversation={conversation}
+                          user={user}
+                          isMe={me._id === user._id}
+                        />
+                      </div>
+                    );
+                  }}
+                </List>
+              )}
+            </>
+          )}
+        </Paper>
       </Modal>
     </div>
   );
