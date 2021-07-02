ba41a972d8fb74539cd14060c8fbf5e51fda7797
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:25:50 +0400

    Add update profile, update avatar, update is away and logout in auth api

--- src/services/api/authApi.ts
+++ src/services/api/authApi.ts
@@ -1,3 +1,4 @@
+import { IEditProfileForm } from "./../../components/EditProfileModal";
 import axios from "axios";
 import { IResponse } from "./types";
 import { IUser } from "../../store/modules/user/types";
@@ -18,19 +19,48 @@ export const authApi = {
     password,
     password2,
   }: IGetStartedForm): Promise<IUser> {
-    const response = await axios.post<IResponse<IUser>>(
-      `/api/auth/register`,
-      {
-        name,
-        email,
-        password,
-        password2,
-      }
-    );
+    const response = await axios.post<IResponse<IUser>>(`/api/auth/register`, {
+      name,
+      email,
+      password,
+      password2,
+    });
     return response.data.data;
   },
   async getMe(): Promise<IUser> {
     const response = await axios.get<IResponse<IUser>>(`/api/auth/me`);
+    return response.data.data;
+  },
+  async updateProfile({
+    name,
+    display_name,
+    work,
+    phone,
+  }: IEditProfileForm): Promise<IUser> {
+    const response = await axios.post<IResponse<IUser>>(`/api/auth/update`, {
+      name,
+      display_name,
+      work,
+      phone,
+    });
+    return response.data.data;
+  },
+  async updateAvatar(avatar: string): Promise<IUser> {
+    const response = await axios.post<IResponse<IUser>>(`/api/auth/update`, {
+      avatar,
+    });
+    return response.data.data;
+  },
+
+  async updateIsAway(away: boolean): Promise<IUser> {
+    const response = await axios.post<IResponse<IUser>>(`/api/auth/update`, {
+      away,
+    });
+    return response.data.data;
+  },
+
+  async logout(): Promise<boolean> {
+    const response = await axios.post<IResponse<boolean>>(`/api/auth/logout`);
     return response.data.data;
   },
 };
