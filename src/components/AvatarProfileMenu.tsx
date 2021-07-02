0748650df91043004388ddd32aff0e15ac2630ca
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 17:44:44 +0400

    Add buttons functionals in avatar profile menu

--- src/components/AvatarProfileMenu.tsx
+++ src/components/AvatarProfileMenu.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 
-import { StyledBadge } from "./StyledBadge";
 import { EditProfileModal } from "./EditProfileModal";
 
 import Paper from "@material-ui/core/Paper/Paper";
 import Box from "@material-ui/core/Box/Box";
-import Avatar from "@material-ui/core/Avatar/Avatar";
 import Typography from "@material-ui/core/Typography/Typography";
 import Divider from "@material-ui/core/Divider/Divider";
 import MenuItem from "@material-ui/core/MenuItem/MenuItem";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import { IUser } from "../store/modules/user/types";
+import { AvatarWithBadge } from "./AvatarWithBadge";
+import { useDispatch } from "react-redux";
+import { logoutUser, updateIsAway } from "../store/modules/user/user";
+import { fetchInfoSideProfile } from "../store/modules/infoSide/infoSide";
 
 interface IAvatarProfileMenuProps {
-  user: IUser;
+  me: IUser;
 }
 
 const useStyles = makeStyles(() =>
@@ -30,49 +32,51 @@ const useStyles = makeStyles(() =>
 );
 
 export const AvatarProfileMenu: React.FC<IAvatarProfileMenuProps> = ({
-  user,
+  me,
 }) => {
   const classes = useStyles();
 
-  /*const handleViewProfileClick = () => {
+  const dispatch = useDispatch();
 
-  };*/
+  const handleSetAwayClick = () => {
+    dispatch(updateIsAway(!me.away));
+  };
+
+  const handleViewProfileClick = () => {
+    dispatch(fetchInfoSideProfile(me._id));
+  };
+
+  const handleSignOutClick = () => {
+    dispatch(logoutUser());
+  };
 
   return (
     <Paper className={classes.submenuContainer}>
       <Box className={classes.avatarMenuHeader}>
-        <StyledBadge
-          overlap="circle"
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right",
-          }}
-          variant="dot"
-        >
-          <Avatar src={user.avatar} />
-        </StyledBadge>
-        <Typography variant="subtitle2">{user.name}</Typography>
-        <Typography variant="caption">Active</Typography>
+        <AvatarWithBadge user={me} />
+        <Typography variant="subtitle2">{me.name}</Typography>
+        <Typography variant="caption">{me.away ? "Away" : "Active"}</Typography>
       </Box>
       <Divider />
-      <MenuItem>
+      <MenuItem button onClick={handleSetAwayClick}>
         <Typography variant="body1">
-          Set yourself as <b>away</b>
+          Set yourself as {me.away ? <b>online</b> : <b>away</b>}
         </Typography>
       </MenuItem>
       <Divider />
       <EditProfileModal
+        me={me}
         opener={
           <MenuItem>
             <Typography variant="body1">Edit profile</Typography>
           </MenuItem>
         }
       />
-      <MenuItem button >
+      <MenuItem button onClick={handleViewProfileClick}>
         <Typography variant="body1">View profile</Typography>
       </MenuItem>
       <Divider />
-      <MenuItem>
+      <MenuItem button onClick={handleSignOutClick}>
         <Typography variant="body1">Sign out of Test company</Typography>
       </MenuItem>
     </Paper>
