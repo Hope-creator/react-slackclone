961c840864265175f49f55c6cd8d2bc8833bf00d
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:22:06 +0400

    Refactor users workspace

--- src/components/Workspace/Members/UsersWorkspace.tsx
+++ src/components/Workspace/Members/UsersWorkspace.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { WorkspaceContent } from "../WorkspaceContent";
 import { WorkspaceHeader } from "../WorkspaceHeader";
 import { UsersContent } from "./Content/UsersContent";
 import { useSelector } from "react-redux";
 import {
-  selectCurrentUsersTotalCount
+  selectCurrentUsersTotalCount,
+  selectIsCurrentUsersLoaded,
 } from "../../../store/modules/currentUsers/selectors";
 
 export const UsersWorkspace = () => {
-
   const totalUserCount = useSelector(selectCurrentUsersTotalCount);
+  const isCurrentUsersLoaded = useSelector(selectIsCurrentUsersLoaded);
 
   return (
     <>
@@ -21,21 +21,13 @@ export const UsersWorkspace = () => {
           <>
             <Typography variant="h6">People</Typography>
             <Typography variant="caption">
-              {totalUserCount} Users
+              {isCurrentUsersLoaded && totalUserCount} Users
             </Typography>
           </>
         }
-        rightSideContent={
-          <>
-            <Button>Invite people</Button>
-          </>
-        }
+        rightSideContent={<></>}
       />
-      <WorkspaceContent
-        children={
-            <UsersContent/>
-        }
-      />
+      <WorkspaceContent children={<UsersContent />} />
     </>
   );
 };
