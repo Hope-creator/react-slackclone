28270041543a91266eea55e27f3b7752b27c5fe7
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:23:37 +0400

    Make unread count not depend on user and show all unreads messages count in unreads hook

--- src/hooks/useUnreadCount.tsx
+++ src/hooks/useUnreadCount.tsx
@@ -3,41 +3,27 @@ import { messagesApi } from "../services/api/messagesApi";
 import socket from "../services/socket/socket";
 import { IMessage } from "../store/modules/messages/types";
 
-export const useUnreadCount = (id: string) => {
+export const useUnreadCount = () => {
   const [unreadCount, setUnreadCount] = React.useState<number>(0);
 
-  const handleListenerUnread = React.useCallback(
-    (message: IMessage) => {
-      console.log(message);
-      message.dest === id && setUnreadCount((prev) => ++prev);
-    },
-    [id]
-  );
+  const handleListenerUnread = React.useCallback((message: IMessage) => {
+    setUnreadCount((prev) => ++prev);
+  }, []);
 
-  const handleListenerReadOne = React.useCallback(
-    (messageDest: string) => {
-      messageDest === id && setUnreadCount((prev) => --prev);
-    },
-    [id]
-  );
+  const handleListenerReadOne = React.useCallback(() => {
+    setUnreadCount((prev) => --prev);
+  }, []);
 
   const handleListenerReadAll = React.useCallback(() => {
     setUnreadCount(0);
   }, []);
 
-  const handleListenerReadAllConversation = React.useCallback(
-    (conversation: string) => {
-      conversation === id && setUnreadCount(0);
-    },
-    [id]
-  );
-
   React.useEffect(() => {
     messagesApi
-      .getUnreadCount(id)
+      .getUnreadCount()
       .then((unreadNum) => setUnreadCount(unreadNum))
       .catch((err) => console.log(err));
-  }, [id]);
+  }, []);
 
   React.useEffect(() => {
     socket.on("SERVER:NEW_UNREAD", handleListenerUnread);
@@ -59,19 +45,6 @@ export const useUnreadCount = (id: string) => {
       socket.removeEventListener("SERVER:READ_ALL", handleListenerReadAll);
     };
   }, [handleListenerReadAll]);
-
-  React.useEffect(() => {
-    socket.on(
-      "SERVER:READ_ALL_CONVERSATION",
-      handleListenerReadAllConversation
-    );
-    return function cleanUp() {
-      socket.removeEventListener(
-        "SERVER:READ_ALL",
-        handleListenerReadAllConversation
-      );
-    };
-  }, [handleListenerReadAllConversation]);
 
   return unreadCount;
 };
