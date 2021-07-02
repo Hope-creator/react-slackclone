19152ea823690522749b6f6cdee5bea4cec6ba52
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:21:29 +0400

    Add message deleted socket handler in marked workspace

--- src/components/Workspace/Marked/MarkedWorkspace.tsx
+++ src/components/Workspace/Marked/MarkedWorkspace.tsx
@@ -2,17 +2,20 @@ import React from "react";
 
 import { WorkspaceContent } from "../WorkspaceContent";
 import { WorkspaceHeader } from "../WorkspaceHeader";
-import { LeftSideMarked } from "./Header/LeftSideMarked";
+import { HeaderLeftMarked } from "./Header/HeaderLeftMarked";
 import { IUser } from "../../../store/modules/user/types";
 import { useDispatch, useSelector } from "react-redux";
 import {
   clearMessagesState,
+  deleteOneMessage,
   fetchMessagesMarked,
 } from "../../../store/modules/messages/messages";
 import {
+  selectIsMessagesLoaded,
   selectMarkedMessages,
 } from "../../../store/modules/messages/selectors";
 import { MarkedContent } from "./Content/MarkedContent";
+import socket from "../../../services/socket/socket";
 
 interface IMarkedWorkspaceProps {
   user: IUser;
@@ -21,6 +24,7 @@ interface IMarkedWorkspaceProps {
 export const MarkedWorkspace: React.FC<IMarkedWorkspaceProps> = ({ user }) => {
   const dispatch = useDispatch();
   const messages = useSelector(selectMarkedMessages);
+  const isMessagesLoaded = useSelector(selectIsMessagesLoaded);
 
   React.useEffect(() => {
     dispatch(fetchMessagesMarked());
@@ -30,16 +34,36 @@ export const MarkedWorkspace: React.FC<IMarkedWorkspaceProps> = ({ user }) => {
     };
   }, [dispatch]);
 
+  const deleteMessageHandleListener = React.useCallback(
+    (messageId: string) => {
+      dispatch(deleteOneMessage(messageId));
+    },
+    [dispatch]
+  );
+
+  React.useEffect(() => {
+    if (isMessagesLoaded) {
+      socket.addEventListener(
+        "SERVER:MESSAGE_DELETED",
+        deleteMessageHandleListener
+      );
+    }
+    return function clearConnect() {
+      socket.removeListener(
+        "SERVER:MESSAGE_DELETED",
+        deleteMessageHandleListener
+      );
+    };
+  }, [deleteMessageHandleListener, isMessagesLoaded]);
+
   return (
     <>
       <WorkspaceHeader
-        leftSideContent={<LeftSideMarked />}
+        leftSideContent={<HeaderLeftMarked />}
         rightSideContent={<></>}
       />
       <WorkspaceContent
-        children={
-            <MarkedContent user={user} messages={messages} />
-        }
+        children={<MarkedContent user={user} messages={messages} />}
       />
     </>
   );
