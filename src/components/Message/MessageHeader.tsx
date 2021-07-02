5f3519c689339df8f287157f6d6b57f7881d91d2
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:06:21 +0400

    Add user popover on message name click

--- src/components/Message/MessageHeader.tsx
+++ src/components/Message/MessageHeader.tsx
@@ -5,10 +5,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import { createStyles } from "@material-ui/core/styles";
 import { format } from "date-fns";
 import Tooltip from "@material-ui/core/Tooltip";
+import { UserPopover } from "../UserPopover";
+import { IUser } from "../../store/modules/user/types";
 
 interface IMessageHeaderProps {
   text: string;
   time: string;
+  user: IUser;
 }
 
 const useStyles = makeStyles(() =>
@@ -27,23 +30,32 @@ const useStyles = makeStyles(() =>
   })
 );
 
-export const MessageHeader: React.FC<IMessageHeaderProps> = ({ text, time }) => {
+export const MessageHeader: React.FC<IMessageHeaderProps> = ({
+  text,
+  time,
+  user,
+}) => {
   const classes = useStyles();
 
   return (
-    <Grid item className={classes.maxWidthText}>
-      <Typography variant="subtitle2">
-        {text}
-        <Tooltip
-          placement="top"
-          classes={{ tooltipPlacementTop: classes.tooltip }}
-          title={format(new Date(time), "PPPppp")}
-        >
-          <Typography className={classes.timeText} variant="caption">
-            {format(new Date(time), "h:mm aa")}
-          </Typography>
-        </Tooltip>
-      </Typography>
+    <Grid alignItems="center" item container className={classes.maxWidthText}>
+      <UserPopover
+        anchorOriginBlockVertical="bottom"
+        anchorOriginBlockHorizontal="center"
+        anchorPopupBlockVertical="top"
+        anchorPopupBlockHorizontal="center"
+        opener={<Typography variant="subtitle2">{text}</Typography>}
+        user={user}
+      />
+      <Tooltip
+        placement="top"
+        classes={{ tooltipPlacementTop: classes.tooltip }}
+        title={format(new Date(time), "PPPppp")}
+      >
+        <Typography className={classes.timeText} variant="caption">
+          {format(new Date(time), "h:mm aa")}
+        </Typography>
+      </Tooltip>
     </Grid>
   );
 };
