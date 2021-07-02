8fd6f021b79f1e11f4414e0b5fdb208d45258802
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:39:17 +0400

    Add update profile , update and remove avatar, logount to user saga

--- src/store/modules/user/operations.ts
+++ src/store/modules/user/operations.ts
@@ -1,5 +1,5 @@
 import { ILoginForm } from "./../../../components/SignInForm";
-import { takeEvery, call, put } from "redux-saga/effects";
+import { takeLatest, call, put } from "redux-saga/effects";
 import { IUser, LoadingUserState } from "./types";
 import {
   fetchMe,
@@ -7,10 +7,19 @@ import {
   setUser,
   setUserLoadingState,
   createUser,
+  updateProfile,
+  updateAvatar,
+  removeAvatar,
+  updateIsAway,
+  logoutUser,
+  clearUserState,
 } from "./user";
 import { PayloadAction } from "@reduxjs/toolkit";
 import { authApi } from "../../../services/api/authApi";
 import { IGetStartedForm } from "../../../components/GetStartedForm";
+import { IEditProfileForm } from "../../../components/EditProfileModal";
+import { uploadApi } from "../../../services/api/uploadApi";
+import socket from "../../../services/socket/socket";
 
 function* fetchMeSaga() {
   try {
@@ -29,8 +38,53 @@ function* fetchUserSaga(action: PayloadAction<ILoginForm>) {
   try {
     const user: IUser = yield call(authApi.signIn, action.payload);
     yield put(setUser(user));
+    yield put(setUserLoadingState(LoadingUserState.LOADED));
   } catch (e) {
     yield put(setUserLoadingState(LoadingUserState.ERRORLOGIN));
+  }
+}
+
+function* updateProfileSaga(action: PayloadAction<IEditProfileForm>) {
+  try {
+    const user: IUser = yield call(authApi.updateProfile, action.payload);
+    yield put(setUser(user));
+    setUserLoadingState(LoadingUserState.LOADED);
+  } catch (e) {
+    yield put(setUserLoadingState(LoadingUserState.ERRORUPDATE));
+  }
+}
+
+function* updateProfileAvatarSaga(action: PayloadAction<FormData>) {
+  try {
+    const avatarSrc: string = yield call(
+      uploadApi.uploadAvatar,
+      action.payload
+    );
+    const user: IUser = yield call(authApi.updateAvatar, avatarSrc);
+    yield put(setUser(user));
+    setUserLoadingState(LoadingUserState.LOADED);
+  } catch (e) {
+    yield put(setUserLoadingState(LoadingUserState.ERRORUPDATE));
+  }
+}
+
+function* updateProfileIsAwaySaga(action: PayloadAction<boolean>) {
+  try {
+    const user: IUser = yield call(authApi.updateIsAway, action.payload);
+    yield put(setUser(user));
+    setUserLoadingState(LoadingUserState.LOADED);
+  } catch (e) {
+    yield put(setUserLoadingState(LoadingUserState.ERRORUPDATE));
+  }
+}
+
+function* removeProfileAvatarSaga() {
+  try {
+    const user: IUser = yield call(authApi.updateAvatar, "");
+    yield put(setUser(user));
+    setUserLoadingState(LoadingUserState.LOADED);
+  } catch (e) {
+    yield put(setUserLoadingState(LoadingUserState.ERRORUPDATE));
   }
 }
 
@@ -47,8 +101,25 @@ function* createUserSaga(action: PayloadAction<IGetStartedForm>) {
   }
 }
 
+function* logoutUserSaga() {
+  try {
+    const isLoggedOut: boolean = yield call(authApi.logout);
+    if (isLoggedOut) {
+      socket.disconnect();
+      yield put(clearUserState());
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export function* userSaga() {
-  yield takeEvery(fetchUser.type, fetchUserSaga);
-  yield takeEvery(fetchMe.type, fetchMeSaga);
-  yield takeEvery(createUser.type, createUserSaga);
+  yield takeLatest(fetchUser.type, fetchUserSaga);
+  yield takeLatest(fetchMe.type, fetchMeSaga);
+  yield takeLatest(createUser.type, createUserSaga);
+  yield takeLatest(updateProfile.type, updateProfileSaga);
+  yield takeLatest(updateAvatar.type, updateProfileAvatarSaga);
+  yield takeLatest(removeAvatar.type, removeProfileAvatarSaga);
+  yield takeLatest(updateIsAway.type, updateProfileIsAwaySaga);
+  yield takeLatest(logoutUser.type, logoutUserSaga);
 }
