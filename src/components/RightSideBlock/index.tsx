12e9d2f319edb0d0a3d143df30aee6b7bed9bc21
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:08:53 +0400

    Add close right side functional on kick

--- src/components/RightSideBlock/index.tsx
+++ src/components/RightSideBlock/index.tsx
@@ -3,8 +3,11 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import socket from "../../services/socket/socket";
 import { IConversation } from "../../store/modules/conversations/types";
-import { updateCurrentInfoConversation } from "../../store/modules/currentInfo_side/currentInfo";
-import { InfoItemTypeState } from "../../store/modules/currentInfo_side/types";
+import {
+  clearInfoSideConversation,
+  updateInfoSideConversation,
+} from "../../store/modules/infoSide/infoSide";
+import { InfoItemTypeState } from "../../store/modules/infoSide/types";
 import { IUser } from "../../store/modules/user/types";
 import { Accordeons } from "./Accordions/Accordions";
 import { Files } from "./Accordions/Files";
@@ -16,43 +19,57 @@ interface RightSideBlockProps {
   headerSubText?: string;
   type: InfoItemTypeState;
   item: IUser | IConversation;
+  user: IUser;
 }
 
 export const RightSideBlock: React.FC<RightSideBlockProps> = ({
   type,
   item,
+  user
 }) => {
   const dispatch = useDispatch();
 
+
   const conversationUpdateHandleListener = React.useCallback(
     (_conversation: IConversation) => {
+        dispatch(updateInfoSideConversation(_conversation));
+    },
+    [dispatch]
+  );
+
+  const conversationKickedHandleListener = React.useCallback(
+    (_conversation: IConversation) => {
       if (
-        type === InfoItemTypeState.CHANNEL &&
-        item._id === _conversation._id
-      ) {
-        dispatch(updateCurrentInfoConversation(_conversation));
+        _conversation.is_private ) {
+        dispatch(clearInfoSideConversation(_conversation._id));
       }
     },
-    [dispatch, type, item._id]
+    [dispatch]
   );
 
   React.useEffect(() => {
     if (type === InfoItemTypeState.CHANNEL) {
       socket.on("SERVER:CONVERSATION_UPDATE", conversationUpdateHandleListener);
+      socket.on("SERVER:CONVERSATION_KICKED", conversationKickedHandleListener);
     }
     return function clearConnect() {
       socket.removeListener(
         "SERVER:CONVERSATION_UPDATE",
-        conversationUpdateHandleListener
+        conversationKickedHandleListener
       );
     };
-  }, [dispatch, conversationUpdateHandleListener, type]);
+  }, [
+    dispatch,
+    type,
+    conversationUpdateHandleListener,
+    conversationKickedHandleListener,
+  ]);
 
   if (type === InfoItemTypeState.CHANNEL) {
     return (
       <Box>
         <Header headerSubText={item.name} headerTitle="Details" />
-        <ContentButtons channel={item as IConversation} />
+        <ContentButtons me={user} channel={item as IConversation} />
         <Accordeons channel={item as IConversation} />
         <Files />
       </Box>
