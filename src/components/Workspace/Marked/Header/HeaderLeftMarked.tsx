113c6726c8a199943e09551b2da3b0f44da658dd
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-02 18:21:44 +0400

    Rename marked workspace header

--- src/components/Workspace/Marked/Header/HeaderLeftMarked.tsx
+++ src/components/Workspace/Marked/Header/HeaderLeftMarked.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 
-export const LeftSideMarked = () => {
+export const HeaderLeftMarked = () => {
   return (
     <Grid container wrap="nowrap" direction="column">
       <Typography variant="subtitle2" color="initial">
