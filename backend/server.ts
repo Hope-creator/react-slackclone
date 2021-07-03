dc8104cf2a032b85a2303af842a08a70605236be
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-03 17:17:04 +0400

    Add port to env

--- backend/server.ts
+++ backend/server.ts
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 const http = createServer(app);
 const io = createSocket(http);
