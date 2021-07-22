9fde8d496d780743b2139dd5669ddf5f1dc50d4d
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-22 20:21:12 +0400

    test: add user sagas test

---
+++ src/__test__/store/user/operations.test.ts
@@ -0,0 +1,221 @@
+import { authApi } from "../../../services/api/authApi";
+import { uploadApi } from "../../../services/api/uploadApi";
+import { call } from "redux-saga/effects";
+import {
+  fetchMe,
+  fetchUser,
+  setUser,
+  setUserLoadingState,
+  createUser,
+  updateProfile,
+  updateAvatar,
+  removeAvatar,
+  updateIsAway,
+  logoutUser,
+  clearUserState,
+} from "../../../store/modules/user/user";
+import { userSaga } from "../../../store/modules/user/operations";
+import { expectSaga } from "redux-saga-test-plan";
+import { LoadingUserState } from "../../../store/modules/user/types";
+import { throwError } from "redux-saga-test-plan/providers";
+import * as matchers from "redux-saga-test-plan/matchers";
+
+const dummyUser = {
+  away: false,
+  company: { name: "My new company", _id: "T01TE7T5WEV" },
+  conversations: [],
+  createdAt: "2021-07-20T11:35:09.725Z",
+  email: "Test@mail.ru",
+  is_admin: true,
+  name: "Test",
+  online: false,
+  updatedAt: "2021-07-20T11:35:09.725Z",
+  _id: "60f6b4ed0acbf2002bb5d29b",
+};
+
+describe("User sagas tests", () => {
+  describe("fetchMeSaga tests", () => {
+    it("should call api and set user on good response", async () => {
+      return expectSaga(userSaga)
+        .provide([[call(authApi.getMe), dummyUser]])
+        .put(setUser(dummyUser))
+        .dispatch(fetchMe())
+        .silentRun();
+    });
+    it("should call api and dispatch error on error response", async () => {
+      return expectSaga(userSaga)
+        .provide([[call(authApi.getMe), throwError()]])
+        .put(setUserLoadingState(LoadingUserState.ERROR))
+        .dispatch(fetchMe())
+        .silentRun();
+    });
+  });
+  describe("fetchUserSaga tests", () => {
+    it("should call api and login", async () => {
+      return expectSaga(userSaga)
+        .provide([
+          [
+            call(authApi.signIn, { email: "Test@mail.com", password: "Test" }),
+            dummyUser,
+          ],
+        ])
+        .put(setUser(dummyUser))
+        .put(setUserLoadingState(LoadingUserState.LOADED))
+        .dispatch(fetchUser({ email: "Test@mail.com", password: "Test" }))
+        .silentRun();
+    });
+    it("should call api and dispatch error login on error response", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.signIn), throwError()]])
+        .put(setUserLoadingState(LoadingUserState.ERRORLOGIN))
+        .dispatch(fetchUser({ email: "Test@mail.com", password: "Test" }))
+        .silentRun();
+    });
+  });
+  describe("updateProfileSaga tests", () => {
+    it("should call api and set updated user profile", async () => {
+      return expectSaga(userSaga)
+        .provide([[call(authApi.updateProfile, { name: "Test" }), dummyUser]])
+        .put(setUser(dummyUser))
+        .put(setUserLoadingState(LoadingUserState.LOADED))
+        .dispatch(updateProfile({ name: "Test" }))
+        .silentRun();
+    });
+    it("should call api and dispatch error update on bad response", async () => {
+      const error = new Error("error");
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.updateProfile), throwError(error)]])
+        .put(setUserLoadingState(LoadingUserState.ERRORUPDATE))
+        .dispatch(updateProfile({ name: "Test" }))
+        .silentRun();
+    });
+  });
+  describe("updateProfileAvatarSaga tests", () => {
+    it("should call upload api, get src from upload and set src to user avatar,then set new user info with avatar", async () => {
+      const avatarSrc = "some-src.com";
+      const dummyUserWithAvatar = { ...dummyUser, avatar: avatarSrc };
+      return expectSaga(userSaga)
+        .provide([
+          [matchers.call.fn(uploadApi.uploadAvatar), avatarSrc],
+          [matchers.call.fn(authApi.updateAvatar), dummyUserWithAvatar],
+        ])
+        .put(setUser(dummyUserWithAvatar))
+        .put(setUserLoadingState(LoadingUserState.LOADED))
+        .dispatch(updateAvatar(new FormData()))
+        .silentRun();
+    });
+    it("should call api and set error update on bad request", async () => {
+      return expectSaga(userSaga)
+        .provide([
+          [matchers.call.fn(uploadApi.uploadAvatar), throwError()],
+          [matchers.call.fn(authApi.updateAvatar), throwError()],
+        ])
+        .put(setUserLoadingState(LoadingUserState.ERRORUPDATE))
+        .dispatch(updateAvatar(new FormData()))
+        .silentRun();
+    });
+  });
+  describe("updateProfileIsAwaySaga tests", () => {
+    it("should call api set user with new isAwat status", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.updateIsAway), dummyUser]])
+        .put(setUser(dummyUser))
+        .put(setUserLoadingState(LoadingUserState.LOADED))
+        .dispatch(updateIsAway(false))
+        .silentRun();
+    });
+    it("should call api and dispatch error update on bad request", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.updateIsAway), throwError()]])
+        .put(setUserLoadingState(LoadingUserState.ERRORUPDATE))
+        .dispatch(updateIsAway(false))
+        .silentRun();
+    });
+  });
+  describe("removeProfileAvatarSaga tests", () => {
+    it("should call api and set new profile with empty avatar field", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.updateAvatar), dummyUser]])
+        .put(setUser(dummyUser))
+        .put(setUserLoadingState(LoadingUserState.LOADED))
+        .dispatch(removeAvatar())
+        .silentRun();
+    });
+    it("should call api and set error update on bad request", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.updateAvatar), throwError()]])
+        .put(setUserLoadingState(LoadingUserState.ERRORUPDATE))
+        .dispatch(removeAvatar())
+        .silentRun();
+    });
+  });
+  describe("createUserSaga tests", () => {
+    it("should call api and set user on registation", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.signUp), dummyUser]])
+        .put(setUser(dummyUser))
+        .dispatch(
+          createUser({
+            name: "Test",
+            email: "Test@mail.com",
+            password: "Test",
+            password2: "Test",
+          })
+        )
+        .silentRun();
+    });
+    it("should call api and dispatch email is already taken error action", async () => {
+      const error = new Error(
+        "Failed to load resource: the server responded with a status of 409 (Conflict)"
+      );
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.signUp), throwError(error)]])
+        .put(setUserLoadingState(LoadingUserState.ERROREMAIL))
+        .dispatch(
+          createUser({
+            name: "Test",
+            email: "Test@mail.com",
+            password: "Test",
+            password2: "Test",
+          })
+        )
+        .silentRun();
+    });
+    it("should call api and dispatch something got wrong error action", async () => {
+      const error = new Error("Network error 500");
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.signUp), throwError(error)]])
+        .put(setUserLoadingState(LoadingUserState.ERROR))
+        .dispatch(
+          createUser({
+            name: "Test",
+            email: "Test@mail.com",
+            password: "Test",
+            password2: "Test",
+          })
+        )
+        .silentRun();
+    });
+  });
+  describe("logoutUserSaga tests", () => {
+    it("should call api and logout user on true response", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.logout), true]])
+        .put(clearUserState())
+        .dispatch(logoutUser())
+        .silentRun();
+    });
+    it("should call api and not doing anything on false response", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.logout), false]])
+        .dispatch(logoutUser())
+        .silentRun();
+    });
+    it("should call api and console log error if api request fails", async () => {
+      return expectSaga(userSaga)
+        .provide([[matchers.call.fn(authApi.logout), throwError()]])
+        .dispatch(logoutUser())
+        .silentRun();
+    });
+  });
+});
