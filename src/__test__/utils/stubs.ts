79afb637961b68638740e6a89f6ffa30d2ca8e1d
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-26 17:59:18 +0400

    test: create stubUser util

---
+++ src/__test__/utils/stubs.ts
@@ -0,0 +1,16 @@
+const stubUser = {
+  away: false,
+  company: { name: "My new company", _id: "T01TE7T5WEV" },
+  conversations: [],
+  createdAt: "2021-07-20T11:35:09.725Z",
+  email: "Test@mail.com",
+  is_admin: true,
+  name: "Test",
+  online: false,
+  updatedAt: "2021-07-20T11:35:09.725Z",
+  _id: "60f6b4ed0acbf2002bb5d29b",
+  avatar: "some-src.com",
+  display_name: "TestName",
+};
+
+export { stubUser };
